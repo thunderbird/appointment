26464deccf06eec75c73e926e8faeaa816c81bc8
Andreas Müller <mail@devmount.de>
2022-08-24 11:14:15 +0200

    📜 add comments and set default for subscriber level

--- backend/src/database/schemas.py
+++ backend/src/database/schemas.py
@@ -1,6 +1,12 @@
+"""Module: schemas
+
+Definitions of valid data shapes for database models.
+"""
 from pydantic import BaseModel
 
 
+""" CALENDAR model schemas
+"""
 class CalendarBase(BaseModel):
   url: str
   user: str
@@ -10,6 +16,7 @@ class CalendarBase(BaseModel):
 class CalendarCreate(CalendarBase):
   pass
 
+
 class Calendar(CalendarBase):
   id: int
   owner_id: int
@@ -18,11 +25,13 @@ class Calendar(CalendarBase):
     orm_mode = True
 
 
+""" SUBSCRIBER model schemas
+"""
 class SubscriberBase(BaseModel):
   username: str
   email: str
   name: str | None = None
-  level: int
+  level: int | None = 1
   timezone: int | None = None
 
 
