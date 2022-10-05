762816078837ca9d1401d9145906a3365139061c
Andreas Müller <mail@devmount.de>
2022-10-05 11:59:12 +0200

    ➕ add tailwindcss

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import './assets/main.css';
 
 createApp(App).use(router).mount('#app');
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
