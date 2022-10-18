10b90f8fd6409ec6f4abfafb3cab69a2eea8bcdb
Andreas Müller <mail@devmount.de>
2022-10-18 15:57:31 +0200

    💚 primary button state styles

--- frontend/src/elements/PrimaryButton.vue
+++ frontend/src/elements/PrimaryButton.vue
@@ -1,5 +1,24 @@
 <template>
-  <button class="h-10 text-xl font-semibold text-white rounded-full bg-gradient-to-br from-teal-400 to-sky-600 disabled:from-teal-200 disabled:to-sky-300 px-6 transition-all ease-in-out">
+  <button class="
+    h-10
+    text-xl
+    font-semibold
+    text-white
+    rounded-full
+    bg-gradient-to-br
+    from-teal-400
+    to-sky-600
+    hover:from-sky-400
+    hover:to-teal-600
+    hover:shadow-md
+    hover:scale-105
+    active:scale-95
+    disabled:from-teal-200
+    disabled:to-sky-300
+    px-6
+    transition-all
+    ease-in-out
+  ">
     {{ label }}
   </button>
 </template>
