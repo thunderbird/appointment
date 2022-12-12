6ea0f005005a8ceadd0193c30bd5cd535b635b9a
Andreas Müller <mail@devmount.de>
2022-12-12 16:41:23 +0100

    🔨 fix z index for event popup

--- frontend/src/elements/EventPopup.vue
+++ frontend/src/elements/EventPopup.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="absolute p-3 bg-white -translate-y-1/2 transition-all shadow-lg rounded-md">
+  <div class="absolute p-3 bg-white -translate-y-1/2 transition-all shadow-lg rounded-md z-30">
     <div class="absolute top-1/2 -translate-y-1/2 -left-1.5 rotate-45 w-3 h-3 bg-white"></div>
     <div class="flex flex-col gap-2">
       <div class="text-lg text-teal-500 font-semibold">{{ event?.title }}</div>
