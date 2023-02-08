8959b43a28939c9fb15166c8690980298edb0eef
Andreas Müller <mail@devmount.de>
2023-02-08 11:08:55 +0100

    🔨 fix config for dev hot reloading

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,5 +3,5 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   productionSourceMap: false,
-	publicPath: '/',
+  publicPath: '/',
 });
