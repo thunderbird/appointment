eb4a8f2074973dd0fbbd5ddeebf88f099db8c494
Andreas Müller <mail@devmount.de>
2023-02-11 18:01:15 +0100

    ➕ waiting state for secondary button

--- frontend/src/elements/PrimaryButton.vue
+++ frontend/src/elements/PrimaryButton.vue
@@ -1,9 +1,10 @@
 <template>
-  <button class="
-    relative h-10 text-xl font-semibold whitespace-nowrap rounded-full bg-gradient-to-br hover:shadow-md hover:scale-102 active:scale-98 disabled:scale-100 disabled:shadow-none px-6 transition-all ease-in-out flex items-center justify-center gap-2
-    text-white from-teal-400 to-sky-600 hover:from-sky-400 hover:to-teal-600 disabled:from-teal-200 disabled:to-sky-300
-  "
-  :class="{ 'text-transparent': waiting }"
+  <button
+    class="
+      relative h-10 text-xl font-semibold whitespace-nowrap rounded-full bg-gradient-to-br hover:shadow-md hover:scale-102 active:scale-98 disabled:scale-100 disabled:shadow-none px-6 transition-all ease-in-out flex items-center justify-center gap-2
+      text-white from-teal-400 to-sky-600 hover:from-sky-400 hover:to-teal-600 disabled:from-teal-200 disabled:to-sky-300
+    "
+    :class="{ 'text-transparent': waiting }"
   >
     <div v-if="waiting" class="absolute w-5 h-5 animate-spin rounded-full border-2 border-white border-t-transparent"></div>
     <copy-icon v-if="icon === 'copy'" class="h-6 w-6 stroke-2 stroke-current fill-transparent" />
@@ -21,8 +22,8 @@ import {
 
 // component properties
 defineProps({
-  label: String,    // button text
-  icon:  String,    // optional icon displayed before label
+  label:   String,  // button text
+  icon:    String,  // optional icon displayed before label
   waiting: Boolean, // if true, spinning animation is shown instead of label
 });
 </script>
--- frontend/src/elements/SecondaryButton.vue
+++ frontend/src/elements/SecondaryButton.vue
@@ -1,23 +1,12 @@
 <template>
-  <button class="
-    h-10
-    text-xl
-    font-semibold
-    whitespace-nowrap
-    text-gray-600
-    bg-white
-    border
-    border-teal-500
-    rounded-full
-    hover:shadow-md
-    hover:scale-102
-    active:scale-98
-    disabled:from-teal-200
-    disabled:to-sky-300
-    px-6
-    transition-all
-    ease-in-out
-  ">
+  <button
+    class="
+      relative h-10 text-xl font-semibold whitespace-nowrap border rounded-full hover:shadow-md hover:scale-102 active:scale-98 px-6 transition-all ease-in-out
+      text-gray-600 bg-white border-teal-500 disabled:from-teal-200 disabled:to-sky-300
+    "
+    :class="{ '!text-transparent': waiting }"
+  >
+    <div v-if="waiting" class="absolute w-5 h-5 animate-spin rounded-full border-2 border-teal-500 border-t-transparent"></div>
     {{ label }}
   </button>
 </template>
@@ -25,6 +14,7 @@
 <script setup>
 // component properties
 defineProps({
-  label: String // button text
+  label:   String,  // button text
+  waiting: Boolean, // if true, spinning animation is shown instead of label
 });
 </script>
