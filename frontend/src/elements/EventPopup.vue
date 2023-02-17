2abc553a81ce13228417d7e536ebbc5e1f3601de
Andreas Müller <mail@devmount.de>
2023-02-17 10:31:31 +0100

    🔨 handle long titles in event popup

--- frontend/src/elements/EventPopup.vue
+++ frontend/src/elements/EventPopup.vue
@@ -2,7 +2,7 @@
   <div class="absolute p-3 bg-white -translate-y-1/2 transition-all shadow-lg rounded-md z-30">
     <div class="absolute top-1/2 -translate-y-1/2 -left-1.5 rotate-45 w-3 h-3 bg-white"></div>
     <div class="flex flex-col gap-2">
-      <div class="text-lg text-teal-500 font-semibold">{{ event?.title }}</div>
+      <div class="text-lg text-teal-500 font-semibold max-w-sm truncate">{{ event?.title }}</div>
       <div class="text-xs text-gray-700 flex gap-1.5 items-center">
         <icon-clock class="h-4 w-4 stroke-teal-500 stroke-2 fill-transparent" />
         {{ eventDateTime }}
