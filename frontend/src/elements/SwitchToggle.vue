720103674f32a24a2044131e5803b511ea0967bc
Andreas Müller <mail@devmount.de>
2023-02-20 16:43:07 +0100

    🔨 fix toggle state

--- frontend/src/elements/SwitchToggle.vue
+++ frontend/src/elements/SwitchToggle.vue
@@ -4,14 +4,16 @@
     :class="{
       'cursor-pointer': !disabled,
     }"
-    @click="!disabled ? state = !state : null"
+    @click.prevent="toggleState"
   >
     <div v-if="label">{{ label }}</div>
     <div class="flex gap-2">
       <div class="text-gray-500">{{ t('label.off') }}</div>
-      <div class="w-10 h-5 shrink-0 bg-gray-300 rounded-full ease-in-out">
+      <div class="w-10 h-5 shrink-0 rounded-full ease-in-out bg-gray-300 dark:bg-gray-600">
         <input type="checkbox" class="hidden peer" :checked="state" />
-        <icon-circle-dot-filled class="w-5 h-5 rounded-full text-gray-400 peer-checked:text-teal-500 duration-300 peer-checked:translate-x-5" />
+        <icon-circle-dot-filled
+          class="w-5 h-5 rounded-full duration-300 peer-checked:translate-x-5 text-gray-400 dark:text-gray-300 peer-checked:text-teal-500"
+        />
       </div>
       <div class="text-gray-500">{{ t('label.on') }}</div>
     </div>
@@ -19,7 +21,7 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import { useI18n } from 'vue-i18n';
 
 // icons
@@ -30,11 +32,19 @@ const { t } = useI18n();
 
 // component properties
 const props = defineProps({
-  active:   Boolean, // toggle state
+  active:   Boolean, // initial toggle state
   disabled: Boolean, // flag for making toggle non changable
   label:    String,  // input label
 });
 
 // current state
-const state = ref(props.active);
+const state = ref(false);
+onMounted(() => {
+  state.value = props.active
+});
+const toggleState = () => {
+  if (!props.disabled) {
+    state.value = !state.value;
+  }
+}
 </script>
