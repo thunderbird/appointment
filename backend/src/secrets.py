e300c4c14cc6e087149304c72d8706bb05825845
Mel <97147377+MelissaAutumn@users.noreply.github.com>
2023-05-05 14:12:23 -0700

    Fix imports potentially having different environment variables. (#43)
    - database schema was imported before we normalize aws secrets
    - Remove load_dotenv on non-dev environments
    - Rip out any mention of sqlite as a default value

--- backend/src/secrets.py
+++ backend/src/secrets.py
@@ -13,7 +13,6 @@ def normalize_secrets():
         os.environ['AUTH0_API_CLIENT_ID'] = secrets.get('client_id')
         os.environ['AUTH0_API_SECRET'] = secrets.get('secret')
         os.environ['AUTH0_API_AUDIENCE'] = secrets.get('audience')
-        os.environ['AUTH0_SECRETS'] = ''
 
     database_secrets = os.getenv('DATABASE_SECRETS')
 
@@ -21,7 +20,6 @@ def normalize_secrets():
         secrets = json.loads(database_secrets)
 
         os.environ['DATABASE_URL'] = f"mysql+mysqldb://{secrets['username']}:{secrets['password']}@{secrets['host']}:{secrets['port']}/appointment"
-        os.environ['DATABASE_SECRETS'] = ''
 
     database_enc_secret = os.getenv('DB_ENC_SECRET')
 
@@ -29,7 +27,6 @@ def normalize_secrets():
         secrets = json.loads(database_enc_secret)
 
         os.environ['DB_SECRET'] = secrets.get('secret')
-        os.environ['DB_ENC_SECRET'] = ''
 
     smtp_secrets = os.getenv('SMTP_SECRETS')
 
@@ -42,7 +39,6 @@ def normalize_secrets():
         os.environ['SMTP_USER'] = secrets.get('username')
         os.environ['SMTP_PASS'] = secrets.get('password')
         os.environ['SMTP_SENDER'] = secrets.get('sender')
-        os.environ['SMTP_SECRETS'] = ''
 
     google_oauth_secrets = os.getenv('GOOGLE_OAUTH_SECRETS')
 
@@ -53,4 +49,3 @@ def normalize_secrets():
         os.environ['GOOGLE_AUTH_SECRET'] = secrets.get('secret')
         os.environ['GOOGLE_AUTH_PROJECT_ID'] = secrets.get('project_id')
         os.environ['GOOGLE_AUTH_CALLBACK'] = secrets.get('callback_url')
-        os.environ['GOOGLE_OAUTH_SECRETS'] = ''
