51d160af038265ec98bae5282634d44afd17b51e
Andreas <mail@devmount.de>
2023-05-08 20:10:48 +0200

    Subscriber out scheme and profile page styles (#40)
    * 🔨 exclude sensitive data from subscriber output
    
    * 💚 give profile page some style

--- frontend/src/views/ProfileView.vue
+++ frontend/src/views/ProfileView.vue
@@ -1,21 +1,43 @@
 <template>
   <!-- page title area -->
-  <div class="flex justify-between items-start select-none">
-    <div class="text-4xl font-light">{{ t('heading.yourProfile') }}</div>
+  <div v-if="user" class="flex flex-col gap-2 justify-center items-center">
+    <div class="text-4xl font-light">{{ user.name }}</div>
+    <div class="flex items-center gap-4">
+      <div class="rounded-full text-xs uppercase border border-gray-500 text-gray-500 px-2">
+        {{ keyByValue(subscriberLevels, user.level) }}
+      </div>
+      <div class="flex gap-1 text-gray-500">
+        {{ user.timezone }}
+        <router-link :to="{ name: 'settings' }" class="pt-0.5 cursor-pointer">
+          <icon-pencil class="w-4 h-4 stroke-1.5" />
+        </router-link>
+      </div>
+    </div>
+    <div class="grid grid-cols-2 mt-8 mb-12 gap-8">
+      <!-- calendars -->
+      <div class="flex flex-col items-center">
+        <div class="text-3xl font-semibold">{{ calendars.length }}/&infin;</div>
+        <div class="text-gray-500 text-center">{{ t('heading.calendarsConnected') }}</div>
+      </div>
+      <!-- appointments -->
+      <div class="flex flex-col items-center">
+        <div class="text-3xl font-semibold">{{ pendingAppointments.length }}</div>
+        <div class="text-gray-500 text-center">{{ t('heading.pendingAppointments') }}</div>
+      </div>
+    </div>
+    <primary-button :label="t('label.logOut')" @click="logout()" />
   </div>
-  <div class="grid grid-cols-2 my-4">
-    <template v-for="(value, key) in user" :key="key">
-      <pre>{{ key }}</pre>
-      <div>{{ value }}</div>
-    </template>
-  </div>
-  <primary-button :label="t('label.logOut')" @click="logout()" />
 </template>
 
 <script setup>
-import { inject, onMounted } from 'vue';
+import { subscriberLevels, appointmentState } from '@/definitions';
+import { inject, computed, onMounted } from 'vue';
+import { keyByValue } from '@/utils';
 import { useI18n } from 'vue-i18n';
 import PrimaryButton from '@/elements/PrimaryButton';
+
+// icons
+import { IconPencil } from '@tabler/icons-vue';
 
 // component constants
 const auth = inject('auth');
@@ -25,10 +47,15 @@ const { t } = useI18n();
 const refresh = inject('refresh');
 
 // view properties
-defineProps({
+const props = defineProps({
   calendars:    Array,  // list of calendars from db
   appointments: Array,  // list of appointments from db
   user:         Object, // currently logged in user, null if not logged in
+});
+
+// list of pending appointments
+const pendingAppointments = computed(() => {
+  return props.appointments.filter(a => a.status === appointmentState.pending)
 });
 
 // do log out
