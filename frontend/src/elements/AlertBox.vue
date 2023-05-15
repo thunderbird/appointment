19f84cb5bc47d0f08bf18c6d874d37d9ecab0197
Mel <97147377+MelissaAutumn@users.noreply.github.com>
2023-05-15 11:19:47 -0700

    Import IconX instead of inlining it.
    Co-authored-by: Andreas <mail@devmount.de>

--- frontend/src/elements/AlertBox.vue
+++ frontend/src/elements/AlertBox.vue
@@ -3,7 +3,7 @@
   <strong class="font-bold">{{ title }}:</strong>
   <span class="block sm:inline ml-1"><slot></slot></span>
   <span class="absolute top-0 bottom-0 right-0 px-4 py-3" @click="onClose">
-    <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+    <icon-x class="h-6 w-6 stroke-1 fill-transparent stroke-red-500" />
   </span>
 </div>
 </template>
