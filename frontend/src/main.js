e19c6f4848f12e8294f7095a207f7d236e8104ea
Melissa Autumn <melissa@thunderbird.net>
2023-05-15 08:40:11 -0700

    Fix router issues regarding auth0 and refreshing the page
    (Accidental fix, so not issue #!)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -37,6 +37,8 @@ app.use(
   createAuth0({
     domain: process.env.VUE_APP_AUTH0_DOMAIN,
     clientId: process.env.VUE_APP_AUTH0_CLIENT_ID,
+    useRefreshTokens: true,
+    cacheLocation: 'localstorage',
     authorizationParams: {
       redirect_uri: window.location.origin,
       audience: process.env.VUE_APP_AUTH0_AUDIENCE,
