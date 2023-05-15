2e1b903ccfa0b9bdc148396fbffd2c0e976e4052
Melissa Autumn <melissa@thunderbird.net>
2023-05-15 11:24:53 -0700

    Import the icon

--- frontend/src/elements/AlertBox.vue
+++ frontend/src/elements/AlertBox.vue
@@ -3,13 +3,14 @@
   <strong class="font-bold">{{ title }}:</strong>
   <span class="block sm:inline ml-1"><slot></slot></span>
   <span class="absolute top-0 bottom-0 right-0 px-4 py-3" @click="onClose">
-    <icon-x class="h-6 w-6 stroke-1 fill-transparent stroke-red-500" />
+    <icon-x class="h-6 w-6 stroke-1 fill-transparent stroke-red-500 cursor-pointer" />
   </span>
 </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
+import { IconX } from '@tabler/icons-vue';
 
 defineProps({
   title: String,
