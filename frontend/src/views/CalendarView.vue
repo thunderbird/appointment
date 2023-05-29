17b12653c764fbe04ffe557b742e520aef2bd423
Solange <solange@thunderbird.net>
2023-05-29 11:22:21 -0700

    Today button size change

--- frontend/src/views/CalendarView.vue
+++ frontend/src/views/CalendarView.vue
@@ -1,6 +1,8 @@
 <template>
   <!-- page title area -->
-  <div class="flex flex-col lg:flex-row justify-between items-start select-none">
+  <div
+    class="flex flex-col lg:flex-row justify-between items-start select-none"
+  >
     <calendar-page-heading
       :nav="true"
       :month="activeDate.format('MMMM')"
@@ -9,11 +11,21 @@
       @prev="dateNav('auto', false)"
       @next="dateNav('auto')"
     />
-    <div class="flex flex-col gap-8 md:flex-row mx-auto lg:ml-0 lg:mr-0 items-center">
-      <button @click="selectDate(dj())" class="font-semibold text-xl text-teal-500 px-4">
-        {{ t('label.today') }}
+    <div
+      class="flex flex-col gap-8 md:flex-row mx-auto lg:ml-0 lg:mr-0 items-center"
+    >
+      <button
+        @click="selectDate(dj())"
+        class="font-semibold text-base text-teal-500 px-4"
+      >
+        {{ t("label.today") }}
       </button>
-      <tab-bar :tab-items="calendarViews" :active="tabActive" @update="updateTab" class="text-xl" />
+      <tab-bar
+        :tab-items="calendarViews"
+        :active="tabActive"
+        @update="updateTab"
+        class="text-xl"
+      />
       <primary-button
         :label="t('label.createAppointments')"
         :disabled="!calendars.length || creationStatus !== creationState.hidden"
@@ -50,7 +62,10 @@
     />
     <!-- page side bar -->
     <div class="w-full sm:w-1/2 md:w-1/5 mx-auto mb-10 md:mb-0 min-w-[310px]">
-      <div v-if="creationStatus === creationState.hidden" class="flex flex-col gap-8">
+      <div
+        v-if="creationStatus === creationState.hidden"
+        class="flex flex-col gap-8"
+      >
         <!-- monthly mini calendar -->
         <calendar-month
           :selected="activeDate"
@@ -64,27 +79,37 @@
         <!-- appointments and events list -->
         <div>
           <div class="flex justify-between items-center">
-            <div class="font-semibold text-lg">{{ t('heading.pendingAppointments') }}</div>
+            <div class="font-semibold text-lg">
+              {{ t("heading.pendingAppointments") }}
+            </div>
             <router-link
               class="px-2 py-1 border-r rounded-full text-xs uppercase bg-teal-500 text-white"
               :to="{ name: 'appointments' }"
             >
-              {{ t('label.viewAll') }}
+              {{ t("label.viewAll") }}
             </router-link>
           </div>
           <div
             v-if="pendingAppointments.length === 0"
             class="mt-4 flex flex-col gap-8 justify-center items-center text-gray-500"
           >
-            <div class="text-center mt-4">{{ t('info.noPendingAppointmentsInList') }}</div>
+            <div class="text-center mt-4">
+              {{ t("info.noPendingAppointmentsInList") }}
+            </div>
             <primary-button
               :label="t('label.createAppointments')"
-              :disabled="!calendars.length || creationStatus !== creationState.hidden"
+              :disabled="
+                !calendars.length || creationStatus !== creationState.hidden
+              "
               @click="creationStatus = creationState.details"
             />
           </div>
           <div v-else class="flex flex-col gap-8 mt-4">
-            <appointment-list-item v-for="a in pendingAppointments" :key="a.id" :appointment="a" />
+            <appointment-list-item
+              v-for="a in pendingAppointments"
+              :key="a.id"
+              :appointment="a"
+            />
           </div>
         </div>
       </div>
@@ -96,36 +121,36 @@
         :user="user"
         @start="creationStatus = creationState.details"
         @next="creationStatus = creationState.availability"
-        @create="creationStatus = creationState.finished; refresh();"
+        @create="
+          creationStatus = creationState.finished;
+          refresh();
+        "
         @cancel="creationStatus = creationState.hidden"
       />
     </div>
   </div>
-
 </template>
 
 <script setup>
-import { creationState, calendarViews, appointmentState } from '@/definitions';
-import {
-  ref, inject, computed, watch, onMounted,
-} from 'vue';
-import { useI18n } from 'vue-i18n';
-import { useRoute, useRouter } from 'vue-router';
-import AppointmentCreation from '@/components/AppointmentCreation';
-import AppointmentListItem from '@/elements/AppointmentListItem';
-import CalendarDay from '@/components/CalendarDay';
-import CalendarMonth from '@/components/CalendarMonth';
-import CalendarPageHeading from '@/elements/CalendarPageHeading';
-import CalendarWeek from '@/components/CalendarWeek';
-import PrimaryButton from '@/elements/PrimaryButton';
-import TabBar from '@/components/TabBar';
+import { creationState, calendarViews, appointmentState } from "@/definitions";
+import { ref, inject, computed, watch, onMounted } from "vue";
+import { useI18n } from "vue-i18n";
+import { useRoute, useRouter } from "vue-router";
+import AppointmentCreation from "@/components/AppointmentCreation";
+import AppointmentListItem from "@/elements/AppointmentListItem";
+import CalendarDay from "@/components/CalendarDay";
+import CalendarMonth from "@/components/CalendarMonth";
+import CalendarPageHeading from "@/elements/CalendarPageHeading";
+import CalendarWeek from "@/components/CalendarWeek";
+import PrimaryButton from "@/elements/PrimaryButton";
+import TabBar from "@/components/TabBar";
 
 const { t } = useI18n();
 const route = useRoute();
 const router = useRouter();
-const dj = inject('dayjs');
-const call = inject('call');
-const refresh = inject('refresh');
+const dj = inject("dayjs");
+const call = inject("call");
+const refresh = inject("refresh");
 
 // view properties
 const props = defineProps({
@@ -137,18 +162,24 @@ const props = defineProps({
 // current selected date, if not in route: defaults to now
 const activeDate = ref(route.params.date ? dj(route.params.date) : dj());
 const selectDate = (d) => {
-  router.replace({ name: route.name, params: { view: route.params.view, date: dj(d).format('YYYY-MM-DD') } });
+  router.replace({
+    name: route.name,
+    params: { view: route.params.view, date: dj(d).format("YYYY-MM-DD") },
+  });
   activeDate.value = dj(d);
 };
 
 // date calculations
-const startOfActiveWeek = computed(() => activeDate.value.startOf('week'));
-const endOfActiveWeek = computed(() => activeDate.value.endOf('week'));
+const startOfActiveWeek = computed(() => activeDate.value.startOf("week"));
+const endOfActiveWeek = computed(() => activeDate.value.endOf("week"));
 
 // active menu item for tab navigation of calendar views
 const tabActive = ref(calendarViews[route.params.view]);
 const updateTab = (view) => {
-  router.replace({ name: route.name, params: { view, date: route.params.date ?? dj().format('YYYY-MM-DD') } });
+  router.replace({
+    name: route.name,
+    params: { view, date: route.params.date ?? dj().format("YYYY-MM-DD") },
+  });
   tabActive.value = calendarViews[view];
 };
 
@@ -156,19 +187,23 @@ const updateTab = (view) => {
 const pageTitle = computed(() => {
   switch (tabActive.value) {
     case calendarViews.day:
-      return activeDate.value.format('dddd Do');
+      return activeDate.value.format("dddd Do");
     case calendarViews.week:
-      return `${startOfActiveWeek.value.format('ddd Do')} - ${endOfActiveWeek.value.format('ddd Do')}`;
+      return `${startOfActiveWeek.value.format(
+        "ddd Do"
+      )} - ${endOfActiveWeek.value.format("ddd Do")}`;
     case calendarViews.month:
     default:
-      return '';
+      return "";
   }
 });
 
 // date navigation
-const dateNav = (unit = 'auto', forward = true) => {
-  if (unit === 'auto') {
-    unit = Object.keys(calendarViews).find((key) => calendarViews[key] === tabActive.value);
+const dateNav = (unit = "auto", forward = true) => {
+  if (unit === "auto") {
+    unit = Object.keys(calendarViews).find(
+      (key) => calendarViews[key] === tabActive.value
+    );
   }
   if (forward) {
     selectDate(activeDate.value.add(1, unit));
@@ -181,37 +216,51 @@ const dateNav = (unit = 'auto', forward = true) => {
 const creationStatus = ref(creationState.hidden);
 
 // list of all pending appointments
-const pendingAppointments = computed(() => props.appointments?.filter((a) => a.status === appointmentState.pending));
+const pendingAppointments = computed(() =>
+  props.appointments?.filter((a) => a.status === appointmentState.pending)
+);
 
 // get remote calendar data for current year
 const calendarEvents = ref([]);
 
 const getRemoteEvents = async (from, to) => {
   calendarEvents.value = [];
-  await Promise.all(props.calendars.map(async (calendar) => {
-    const { data } = await call(`rmt/cal/${calendar.id}/${from}/${to}`).get().json();
-    if (Array.isArray(data.value)) {
-      calendarEvents.value.push(...data.value.map((e) => ({ ...e, duration: dj(e.end).diff(dj(e.start), 'minutes') })));
-    }
-  }));
+  await Promise.all(
+    props.calendars.map(async (calendar) => {
+      const { data } = await call(`rmt/cal/${calendar.id}/${from}/${to}`)
+        .get()
+        .json();
+      if (Array.isArray(data.value)) {
+        calendarEvents.value.push(
+          ...data.value.map((e) => ({
+            ...e,
+            duration: dj(e.end).diff(dj(e.start), "minutes"),
+          }))
+        );
+      }
+    })
+  );
 };
 
 // initially load data when component gets remounted
 onMounted(async () => {
   await refresh();
-  const eventsFrom = dj(activeDate.value).startOf('year').format('YYYY-MM-DD');
-  const eventsTo = dj(activeDate.value).endOf('year').format('YYYY-MM-DD');
+  const eventsFrom = dj(activeDate.value).startOf("year").format("YYYY-MM-DD");
+  const eventsTo = dj(activeDate.value).endOf("year").format("YYYY-MM-DD");
   await getRemoteEvents(eventsFrom, eventsTo);
 });
 
 // react to user calendar navigation
-watch(() => activeDate.value, (newValue, oldValue) => {
-  // remote data is retrieved per year, so data request happens only if the user navigates to a different year
-  if (dj(oldValue).format('YYYY') !== dj(newValue).format('YYYY')) {
-    getRemoteEvents(
-      dj(newValue).startOf('year').format('YYYY-MM-DD'),
-      dj(newValue).endOf('year').format('YYYY-MM-DD'),
-    );
+watch(
+  () => activeDate.value,
+  (newValue, oldValue) => {
+    // remote data is retrieved per year, so data request happens only if the user navigates to a different year
+    if (dj(oldValue).format("YYYY") !== dj(newValue).format("YYYY")) {
+      getRemoteEvents(
+        dj(newValue).startOf("year").format("YYYY-MM-DD"),
+        dj(newValue).endOf("year").format("YYYY-MM-DD")
+      );
+    }
   }
-});
+);
 </script>
