f0281ae80bceb4c4d6539c712789441d62afe617
Melissa Autumn <melissa@thunderbird.net>
2023-05-31 09:22:08 -0700

    Lint TitleBar

--- frontend/src/components/TitleBar.vue
+++ frontend/src/components/TitleBar.vue
@@ -1,6 +1,20 @@
 <template>
   <header
-    class="fixed h-16 w-full px-4 shadow-lg border-b flex items-center justify-between cursor-default select-none bg-white dark:bg-gray-700 border-gray-300 dark:border-gray-600"
+    class="fixed
+    h-16
+    w-full
+    px-4
+    shadow-lg
+    border-b
+    flex
+    items-center
+    justify-between
+    cursor-default
+    select-none
+    bg-white
+    dark:bg-gray-700
+    border-gray-300
+    dark:border-gray-600"
   >
     <div class="flex gap-3 items-center">
       <img
@@ -19,9 +33,9 @@
 </template>
 
 <script setup>
-import { useAuth0 } from "@auth0/auth0-vue";
-import { useI18n } from "vue-i18n";
-import PrimaryButton from "@/elements/PrimaryButton";
+import { useAuth0 } from '@auth0/auth0-vue';
+import { useI18n } from 'vue-i18n';
+import PrimaryButton from '@/elements/PrimaryButton';
 
 const { t } = useI18n();
 const auth0 = useAuth0();
