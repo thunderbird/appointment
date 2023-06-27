209c82767a92daea521fd04428485a71936f0022
Chris Aquino <chris@thunderbird.net>
2023-06-27 18:25:36 -0400

    Add migration file for schedule and availability tables

---
+++ backend/src/migrations/versions/2023_06_27_1108-f9660871710e_add_general_availability_tables.py
@@ -0,0 +1,61 @@
+"""add general availability tables
+
+Revision ID: f9660871710e
+Revises: da069f44bca7
+Create Date: 2023-06-27 11:08:39.853063
+
+"""
+import os
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy import DateTime, false
+from sqlalchemy_utils import StringEncryptedType
+from sqlalchemy_utils.types.encrypted.encrypted_type import AesEngine
+from database.models import DayOfWeek
+
+
+def secret():
+    return os.getenv("DB_SECRET")
+
+
+# revision identifiers, used by Alembic.
+revision = "f9660871710e"
+down_revision = "da069f44bca7"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "schedules",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("appointment_id", sa.Integer),
+        sa.Column(
+            "name",
+            StringEncryptedType(sa.String, secret, AesEngine, "pkcs5", length=2048),
+            index=False,
+        ),
+        sa.Column(
+            "slug",
+            StringEncryptedType(sa.String, secret, AesEngine, "pkcs5", length=2048),
+            index=False,
+        ),
+        sa.Column("time_created", DateTime()),
+        sa.Column("time_updated", DateTime()),
+    )
+    op.create_table(
+        "availabilities",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("schedule_id", sa.Integer),
+        sa.Column("day_of_week", sa.Enum(DayOfWeek), default=DayOfWeek.Monday),
+        sa.Column("start_time", DateTime()),
+        sa.Column("end_time", DateTime()),
+        sa.Column("booking_cutoff_duration", sa.Integer),
+        sa.Column("time_created", DateTime()),
+        sa.Column("time_updated", DateTime()),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("schedules")
+    op.drop_table("availabilities")
