b2e3442a76a7cfd34b2baf3c028b8a42fa3d0505
Melissa Autumn <melissa@thunderbird.net>
2023-10-16 13:05:57 -0700

    Add some information on how to get started to the homepage / in an alert banner for Calendars page.

--- frontend/src/elements/AlertBox.vue
+++ frontend/src/elements/AlertBox.vue
@@ -2,11 +2,13 @@
 <div
   class="
     p-2 leading-none lg:rounded-full flex lg:inline-flex items-center shadow-md dark:shadow-lg
-    bg-rose-600 dark:bg-rose-900 text-white shadow-black/30"
+    text-white shadow-black/30"
   role="alert"
-  :class="{ hidden: isHidden }"
+  :class="{ hidden: isHidden, 'error-title': scheme === 'error', 'alert-title': scheme === 'alert' }"
 >
-  <span class="flex rounded-full bg-rose-500 dark:bg-rose-800 uppercase px-2 py-1 text-xs font-bold mr-3">
+  <span class="flex rounded-full uppercase px-2 py-1 text-xs font-bold mr-3"
+    :class="{ 'error-body': scheme === 'error', 'alert-body': scheme === 'alert' }"
+  >
     {{ title }}
   </span>
   <span class="block sm:inline ml-1">
@@ -24,8 +26,28 @@ import { IconX } from '@tabler/icons-vue';
 
 defineProps({
   title: String,
+  scheme: {
+    type: String,
+    default: 'error',
+  },
 });
 
 const isHidden = ref(false);
 const onClose = () => { isHidden.value = true; };
+
 </script>
+
+<style scoped>
+  .error-title {
+    @apply bg-rose-600 dark:bg-rose-900;
+  }
+  .error-body {
+    @apply bg-rose-500 dark:bg-rose-800;
+  }
+  .alert-title {
+    @apply bg-orange-400 dark:bg-orange-700;
+  }
+  .alert-body {
+    @apply bg-orange-500 dark:bg-orange-800;
+  }
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,13 +1,33 @@
 <template>
-  <div class="h-full p-4 flex-center flex-col gap-8 select-none">
+  <div class="h-full p-4 flex-center flex-col gap-8">
     <img class="w-full max-w-md" src="/appointment_logo.svg" alt="Appointment Logo" />
+    <i18n-t keypath="text.homepageGreetingIntro" tag="label" for="text.homepageGreetingIntroLink">
+      <a class="underline" href="https://thunderbird.net" target="_blank">
+        {{ $t('text.homepageGreetingIntroLink') }}
+      </a>
+    </i18n-t>
+    <p v-if="!isAuthenticated">{{ t('text.homepageGreetingUnauthenticated') }}</p>
+    <i18n-t
+      v-if="isAuthenticated"
+      keypath="text.homepageGreetingAuthenticated"
+      tag="label"
+      for="text.homepageGreetingAuthenticatedLink">
+        <a class="underline" href="/settings/calendar" target="_blank">
+          {{ $t('text.homepageGreetingAuthenticatedLink') }}
+        </a>
+    </i18n-t>
   </div>
+
 </template>
 
 <script setup>
-import { inject, onMounted } from 'vue';
+import { computed, inject, onMounted } from 'vue';
+import { useAuth0 } from '@auth0/auth0-vue';
 
 const refresh = inject('refresh');
+
+const auth = useAuth0();
+const isAuthenticated = computed(() => auth.isAuthenticated.value);
 
 // initially load data when component gets remounted
 onMounted(async () => {
