c01280a3bf0605c64e04ef16bee12c63beb0bff4
Andreas Müller <mail@devmount.de>
2023-10-17 17:56:34 +0200

    ❌ remove scoped styles

--- frontend/src/elements/AlertBox.vue
+++ frontend/src/elements/AlertBox.vue
@@ -4,10 +4,17 @@
     p-2 leading-none lg:rounded-full flex lg:inline-flex items-center shadow-md dark:shadow-lg
     text-white shadow-black/30"
   role="alert"
-  :class="{ hidden: isHidden, 'error-title': scheme === 'error', 'alert-title': scheme === 'alert' }"
+  :class="{
+    hidden: isHidden,
+    'bg-rose-600 dark:bg-rose-900': isError,
+    'bg-orange-400 dark:bg-orange-700': isAlert,
+  }"
 >
   <span class="flex rounded-full uppercase px-2 py-1 text-xs font-bold mr-3"
-    :class="{ 'error-body': scheme === 'error', 'alert-body': scheme === 'alert' }"
+    :class="{
+      'bg-rose-500 dark:bg-rose-800': isError,
+      'bg-orange-500 dark:bg-orange-800': isAlert,
+    }"
   >
     {{ title }}
   </span>
@@ -24,7 +31,7 @@
 import { ref } from 'vue';
 import { IconX } from '@tabler/icons-vue';
 
-defineProps({
+const props = defineProps({
   title: String,
   scheme: {
     type: String,
@@ -32,22 +39,10 @@ defineProps({
   },
 });
 
+const isError = computed(() => props.scheme === 'error');
+const isAlert = computed(() => props.scheme === 'alert');
+
 const isHidden = ref(false);
 const onClose = () => { isHidden.value = true; };
 
 </script>
-
-<style scoped>
-  .error-title {
-    @apply bg-rose-600 dark:bg-rose-900;
-  }
-  .error-body {
-    @apply bg-rose-500 dark:bg-rose-800;
-  }
-  .alert-title {
-    @apply bg-orange-400 dark:bg-orange-700;
-  }
-  .alert-body {
-    @apply bg-orange-500 dark:bg-orange-800;
-  }
-</style>
