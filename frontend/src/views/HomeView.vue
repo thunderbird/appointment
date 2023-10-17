2559f2cb4ca10e173b52a3ab80df48032b97ed59
Andreas Müller <mail@devmount.de>
2023-10-17 17:49:48 +0200

    📜 improving texts

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,20 +1,17 @@
 <template>
   <div class="h-full p-4 flex-center flex-col gap-8">
     <img class="w-full max-w-md" src="/appointment_logo.svg" alt="Appointment Logo" />
-    <i18n-t keypath="text.homepageGreetingIntro" tag="label" for="text.homepageGreetingIntroLink">
-      <a class="underline" href="https://thunderbird.net" target="_blank">
-        {{ $t('text.homepageGreetingIntroLink') }}
-      </a>
-    </i18n-t>
+    {{ $t('text.homepageGreetingIntro') }}
     <p v-if="!isAuthenticated">{{ $t('text.homepageGreetingUnauthenticated') }}</p>
     <i18n-t
       v-if="isAuthenticated"
       keypath="text.homepageGreetingAuthenticated"
       tag="label"
-      for="text.homepageGreetingAuthenticatedLink">
-        <a class="underline" href="/settings/calendar" target="_blank">
-          {{ $t('text.homepageGreetingAuthenticatedLink') }}
-        </a>
+      for="text.homepageGreetingAuthenticatedLink"
+    >
+      <a class="underline" href="/settings/calendar">
+        {{ $t('text.homepageGreetingAuthenticatedLink') }}
+      </a>
     </i18n-t>
   </div>
 
