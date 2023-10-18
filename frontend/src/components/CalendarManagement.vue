058eb26736cd57600cca01fcaf121eefe80ad43a
Andreas Müller <mail@devmount.de>
2023-10-18 12:26:56 +0200

    🔨 fix sync button styles

--- frontend/src/components/CalendarManagement.vue
+++ frontend/src/components/CalendarManagement.vue
@@ -2,22 +2,22 @@
   <div class="flex max-w-2xl">
     <div class="text-xl">{{ title }}</div>
     <div class="inline-flex mx-auto mr-0" v-if="type === calendarManagementType.connect">
-      <button
-        class="bg-transparent disabled:scale-100 disabled:shadow-none disabled:opacity-50"
+      <secondary-button
+        class="text-sm !text-teal-500 disabled:scale-100 disabled:shadow-none disabled:opacity-50"
         :disabled="loading"
         @click="emit('sync')"
       >
         <span class="inline-block mr-2">
           {{ t('label.syncCalendars') }}
         </span>
-        <icon-refresh class="inline-block h-5 w-5 stroke-2 stroke-white fill-transparent"></icon-refresh>
-      </button>
+        <icon-refresh class="inline-block h-4 w-4 stroke-2" :class="{ 'animate-spin': loading }" />
+      </secondary-button>
     </div>
   </div>
   <div v-if="filteredCalendars?.length" class="pl-6 flex flex-col gap-2 max-w-2xl">
     <div v-for="cal in filteredCalendars" :key="cal.id" class="flex gap-2 items-center">
       <div class="flex-center w-6 h-6 rounded-lg" :style="{ backgroundColor: cal.color ?? '#38bdf8' }">
-        <icon-calendar class="w-4 h-4 fill-transparent stroke-2 stroke-white"/>
+        <icon-calendar class="w-4 h-4 stroke-2 stroke-white"/>
       </div>
       <span class="calendar-title">
       {{ cal.title }}
@@ -31,7 +31,7 @@
             bg-teal-500 text-white disabled:scale-100 disabled:shadow-none disabled:opacity-50
           "
       >
-        <icon-arrow-right class="h-3 w-3 stroke-2 stroke-white fill-transparent"/>
+        <icon-arrow-right class="h-3 w-3 stroke-3"/>
         {{ t('label.connectCalendar') }}
       </button>
       <button
@@ -43,7 +43,7 @@
             bg-teal-500 text-white disabled:scale-100 disabled:shadow-none disabled:opacity-50
           "
       >
-        <icon-pencil class="h-3 w-3 stroke-2 stroke-white fill-transparent"/>
+        <icon-pencil class="h-3 w-3 stroke-3"/>
         {{ t('label.editCalendar') }}
       </button>
       <button
@@ -52,19 +52,18 @@
         :disabled="loading"
         @click="emit('remove', cal.id)"
       >
-        <icon-x class="h-5 w-5 stroke-2 stroke-red-500 fill-transparent"/>
+        <icon-x class="h-5 w-5 stroke-2 stroke-red-500"/>
       </button>
     </div>
   </div>
 </template>
 
 <script setup>
-import {
-  IconArrowRight, IconCalendar, IconPencil, IconX, IconRefresh,
-} from '@tabler/icons-vue';
 import { calendarManagementType } from '@/definitions';
 import { computed } from 'vue';
+import { IconArrowRight, IconCalendar, IconPencil, IconX, IconRefresh } from '@tabler/icons-vue';
 import { useI18n } from 'vue-i18n';
+import SecondaryButton from '@/elements/SecondaryButton';
 
 const { t } = useI18n({ useScope: 'global' });
 const emit = defineEmits(['modify', 'remove', 'sync']);
