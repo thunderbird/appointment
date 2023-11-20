2db116f467839ebb391715dde0553ed843a5a0f5
Melissa Autumn <melissa@thunderbird.net>
2023-11-20 09:18:35 -0800

    Add the site title to vue config. (Fixes #180)

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -5,6 +5,12 @@ module.exports = defineConfig({
   transpileDependencies: true,
   productionSourceMap: true,
   publicPath: '/',
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'Thunderbird Appointment',
+    },
+  },
   configureWebpack: {
     devtool: 'source-map', // Source map generation must be turned on
     plugins: [
