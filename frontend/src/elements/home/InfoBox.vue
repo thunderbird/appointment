fa9c4d9fdcb9beb40b135a492ecb026256ef4024
Melissa Autumn <melissa@thunderbird.net>
2023-11-22 14:40:25 -0800

    Adjust box shadow to match other box shadow colours in the app

--- frontend/src/elements/home/InfoBox.vue
+++ frontend/src/elements/home/InfoBox.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="flex flex-col mb-20 w-full md:w-1/3 shadow-[0_-5px_5px_-5px_#3B455F19]">
+  <div class="flex flex-col mb-20 w-full md:w-1/3 shadow-[0_-5px_5px_-5px_rgb(0_0_0/0.3)]
+">
     <div class="h-16 flex gap-4 m-4">
       <!--<img class="w-[64px] h-[64px]" src="@/assets/svg/icon-wand.svg"/>-->
       <h3 class="text-2xl text-teal-600 dark:text-teal-400 my-auto">{{ title }}</h3>
