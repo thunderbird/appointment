3fe4dfaa2ca7aa035b529f3ac5510648f689fc04
Andreas <mail@devmount.de>
2023-12-05 20:22:02 +0100

    Features/144 Profile dropdown ga link (#197)
    * ➕ component element for nav bar items
    
    * ➕ provide unified login and logout methods
    
    * 🔨 fix missing import
    
    * ➕ add profile dropdown
    
    * ➕ add ga link to schedule creation area
    
    * 🔨 fix vue warning for extraneous non-props attributes
    
    * 💚 add active nav item color gradients
    
    * 🔨 fix share my link buttons to copy link into clipboard

---
+++ frontend/src/elements/NavBarItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="relative group flex font-medium text-base"
+    :class="{ 'text-gray-600 dark:text-gray-300': !active }"
+  >
+    <router-link class="flex justify-center min-w-[120px] items-center" :to="{ name: linkName }">
+      {{ label }}
+    </router-link>
+    <div
+      class="absolute rounded bottom-0 h-1 w-full bg-transparent transition-colors ease-in-out"
+      :class="{
+        'bg-gradient-to-r from-teal-500 to-sky-500': active,
+        'group-hover:bg-gray-200 group-hover:dark:bg-gray-400': !active,
+      }"
+    ></div>
+  </div>
+</template>
+
+<script setup>
+// component properties
+defineProps({
+  active: Boolean,  // indicator for currently active item
+  label: String,    // item text label
+  linkName: String, // name of the route link target
+});
+</script>
