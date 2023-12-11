5cbb947e9375cfd48bcdf0b11f430120a1e18bdc
Melissa Autumn <melissa@thunderbird.net>
2023-12-11 07:51:05 -0800

    Use base url for web history

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -98,7 +98,7 @@ const routes = [
 
 // create router object to export
 const router = createRouter({
-  history: createWebHistory(''),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
 
