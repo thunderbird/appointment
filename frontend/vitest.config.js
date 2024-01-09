d126bf148146b1cf0b1dfddb5a52a931ff554973
Melissa Autumn <melissa@thunderbird.net>
2024-01-09 11:09:35 -0800

    Add an initial user-store test along with test config files.

---
+++ frontend/vitest.config.js
@@ -0,0 +1,21 @@
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+import viteConfig from './vite.config';
+
+export default defineConfig({
+  viteConfig,
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+    extensions: ['.js', '.vue'],
+  },
+  test: {
+    setupFiles: [
+      '/test/setup/fixFetch.js',
+    ],
+    globals: true,
+    environment: 'jsdom',
+    root: fileURLToPath(new URL('./', import.meta.url)),
+  },
+});
