9aa34db20cc92ae4d4b6052dadf8d1ae92391b35
Melissa Autumn <melissa@thunderbird.net>
2024-01-12 10:03:09 -0800

    Migration merge commit.

---
+++ backend/src/appointment/migrations/versions/2024_01_12_1801-ea551afc14fc_merge_commit.py
@@ -0,0 +1,24 @@
+"""Merge Commit
+
+Revision ID: ea551afc14fc
+Revises: ad7cc2de5ff8, 502c76bc79e0
+Create Date: 2024-01-12 18:01:38.962773
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ea551afc14fc'
+down_revision = ('ad7cc2de5ff8', '502c76bc79e0')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
