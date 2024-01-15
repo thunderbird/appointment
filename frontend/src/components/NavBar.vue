cdbcb1f3c424a0cbc5d405d62ae3d8f5af542f8f
Andreas Müller <mail@devmount.de>
2024-01-15 15:36:08 +0100

    ➕ move navbar avatar to separate component
    (cherry picked from commit 2d5dc901b9f5c1a515307eafd83ff7dda10dd6ad)

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -37,17 +37,7 @@
       </div>
       <drop-down class="self-center">
         <template #trigger>
-          <div
-            class="w-12 h-12 mr-4 self-center flex-center rounded-full text-lg bg-white font-normal text-white"
-            :class="{'bg-teal-500': user.data.avatarUrl === null}"
-          >
-            <span v-if="user.data.avatarUrl === null">
-              {{ initials(user.data.name) }}
-            </span>
-            <span v-else>
-              <img class="rounded-full w-[48px] h-[48px]" :alt="initials(user.data.name)" :src="user.data.avatarUrl"/>
-            </span>
-          </div>
+          <avatar />
         </template>
         <template #default>
           <div class="flex flex-col gap-2 rounded-md w-48 p-4 bg-white dark:bg-gray-700 shadow-md">
@@ -75,10 +65,10 @@
 import { ref, inject, onMounted } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { useRoute, useRouter } from 'vue-router';
-import { initials } from '@/utils';
 import { useUserStore } from '@/stores/user-store';
-import NavBarItem from "@/elements/NavBarItem";
+import Avatar from "@/elements/Avatar";
 import DropDown from "@/elements/DropDown";
+import NavBarItem from "@/elements/NavBarItem";
 import TextButton from "@/elements/TextButton";
 
 // component constants
