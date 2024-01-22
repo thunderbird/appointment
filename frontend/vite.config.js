5a6e24d1d954efa99b6f6755704cbd9d0b4db778
Andreas Müller <mail@devmount.de>
2024-01-22 07:34:00 +0100

    ❌ removy dynamically adding sentry

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,17 +4,14 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-const plugins = [vue()];
-if (import.meta.env?.VITE_SENTRY_DSN) {
-  plugins.push(sentryVitePlugin({
-    org: 'thunderbird',
-    project: 'appointment-frontend',
-  }));
-}
-
 export default defineConfig({
-  plugins: plugins,
-
+  plugins: [
+    vue(),
+    sentryVitePlugin({
+      org: 'thunderbird',
+      project: 'appointment-frontend',
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
