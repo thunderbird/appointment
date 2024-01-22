59bfcd221c2b1deee1dad2a0ea6c86723e71a5bd
Andreas <mail@devmount.de>
2024-01-22 14:36:42 +0100

    Stop using cjs node api (#231)
    * 🔨 stop using cjs node api
    
    * ❌ removy dynamically adding sentry
    
    * 📦 update dependencies
    
    * 👕 fix indentation and comments

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,11 +6,13 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), sentryVitePlugin({
-    org: 'thunderbird',
-    project: 'appointment-frontend',
-  })],
-
+  plugins: [
+    vue(),
+    sentryVitePlugin({
+      org: 'thunderbird',
+      project: 'appointment-frontend',
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
