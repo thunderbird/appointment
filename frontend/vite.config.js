ed3e5b36c52087ec7403bef862c4fdbc909d014a
Andreas Müller <mail@devmount.de>
2024-01-22 14:34:24 +0100

    👕 fix indentation and comments

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
