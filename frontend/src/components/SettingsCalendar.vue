b42f065fa906d2d6ec20e86001423c6a51acae15
Melissa Autumn <melissa@thunderbird.net>
2024-03-05 13:13:39 -0800

    Change sign in with google button to connect google calendar.

--- frontend/src/components/SettingsCalendar.vue
+++ frontend/src/components/SettingsCalendar.vue
@@ -180,7 +180,6 @@
           class="h-[40px] cursor-pointer"
           :alt="t('label.signInWithGoogle')"
           :src="GoogleSignInBtn"
-          :srcset="`${GoogleSignInBtn2x} 2x`"
           @click="saveCalendar"
         />
       </div>
@@ -210,8 +209,7 @@ import { useI18n } from 'vue-i18n';
 import { useRoute, useRouter } from 'vue-router';
 import AlertBox from '@/elements/AlertBox';
 import CalendarManagement from '@/components/CalendarManagement.vue';
-import GoogleSignInBtn from '@/assets/img/google/1x/btn_google_signin_light_normal_web.png';
-import GoogleSignInBtn2x from '@/assets/img/google/2x/btn_google_signin_light_normal_web@2x.png';
+import GoogleSignInBtn from '@/assets/img/google/btn_connect_google_cal.png';
 import PrimaryButton from '@/elements/PrimaryButton';
 import SecondaryButton from '@/elements/SecondaryButton';
 import ConfirmationModal from '@/components/ConfirmationModal.vue';
