1befcb45329daab0a1ef796f6ab11cb0d41a7904
Mel <97147377+MelissaAutumn@users.noreply.github.com>
2024-03-26 08:02:02 -0700

    Create data migration to fill in missing timestamps. (#339)

---
+++ backend/src/appointment/migrations/versions/2024_03_25_2221-bbdfad87a7fb_fix_null_timestamps.py
@@ -0,0 +1,29 @@
+"""[data migration] fix null timestamps
+
+Revision ID: bbdfad87a7fb
+Revises: f92bae6c27da
+Create Date: 2024-03-25 22:21:21.464528
+
+"""
+from alembic import op
+from sqlalchemy.orm import Session
+
+# revision identifiers, used by Alembic.
+revision = 'bbdfad87a7fb'
+down_revision = 'f92bae6c27da'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    session = Session(op.get_bind())
+    tables = ['appointments', 'attendees', 'availabilities', 'calendars', 'external_connections', 'schedules', 'slots',
+              'subscribers']
+
+    for table in tables:
+        session.execute(f'UPDATE {table} SET time_created = NOW() WHERE time_created is NULL')
+        session.execute(f'UPDATE {table} SET time_updated = NOW() WHERE time_updated is NULL')
+
+
+def downgrade() -> None:
+    pass
