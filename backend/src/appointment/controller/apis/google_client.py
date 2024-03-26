9f7fa0fdee0109eae5b1f9a744f362da0e51c8a8
Melissa Autumn <melissa@thunderbird.net>
2024-03-26 13:47:45 -0700

    Import the event instead of inserting it. This prevents the event from being duplicated on your primary google calendar.

--- backend/src/appointment/controller/apis/google_client.py
+++ backend/src/appointment/controller/apis/google_client.py
@@ -135,7 +135,7 @@ class GoogleClient:
         response = None
         with build("calendar", "v3", credentials=token, cache_discovery=False) as service:
             try:
-                response = service.events().insert(calendarId=calendar_id, body=body).execute()
+                response = service.events().import_(calendarId=calendar_id, body=body).execute()
             except HttpError as e:
                 logging.warning(f"[google_client.create_event] Request Error: {e.status_code}/{e.error_details}")
                 raise EventNotCreatedException()
