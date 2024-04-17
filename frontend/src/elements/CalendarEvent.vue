702ba08f6ef5785bb2b39c4bb7cbc4a5758f02ee
Andreas <mail@devmount.de>
2024-04-17 21:24:55 +0200

    💚 Fix unreadable colored text (#365)

--- frontend/src/elements/CalendarEvent.vue
+++ frontend/src/elements/CalendarEvent.vue
@@ -30,7 +30,7 @@
         :style="{
           borderColor: eventColor(eventData, placeholder).border,
           backgroundColor: monthView || placeholder ? eventColor(eventData, placeholder).background : eventData.calendar_color,
-          color: monthView || placeholder ? getAccessibleColor(eventColor(eventData, placeholder).background) : getAccessibleColor(eventData.calendar_color),
+          color: !monthView ? getAccessibleColor(eventData.calendar_color) : null,
         }"
         @click="emit('eventSelected', day)"
         @mouseenter="element => showDetails ? popup=showEventPopup(element, event, popupPosition) : null"
