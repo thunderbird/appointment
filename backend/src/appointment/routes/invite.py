14f91566b7cb8776f4393673c38ed15daa78eb79
Melissa Autumn <melissa@thunderbird.net>
2024-04-17 16:02:42 -0700

    Error out the invite routes for now.
    We need to add proper access control for these.

--- backend/src/appointment/routes/invite.py
+++ backend/src/appointment/routes/invite.py
@@ -14,12 +14,15 @@ router = APIRouter()
 
 @router.post("/generate/{n}", response_model=list[schemas.Invite])
 def generate_invite_codes(n: int, db: Session = Depends(get_db)):
+    raise NotImplementedError
     """endpoint to generate n invite codes"""
     return repo.generate_invite_codes(db, n)
 
 
 @router.put("/redeem/{code}")
 def use_invite_code(code: str, db: Session = Depends(get_db)):
+    raise NotImplementedError
+
     """endpoint to create a new subscriber and update the corresponding invite"""
     if not repo.invite_code_exists(db, code):
         raise validation.InviteCodeNotFoundException()
@@ -33,6 +36,7 @@ def use_invite_code(code: str, db: Session = Depends(get_db)):
 
 @router.put("/revoke/{code}")
 def use_invite_code(code: str, db: Session = Depends(get_db)):
+    raise NotImplementedError
     """endpoint to revoke a given invite code and mark in unavailable"""
     if not repo.invite_code_exists(db, code):
         raise validation.InviteCodeNotFoundException()
