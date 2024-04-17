98b70007407242b79da4445b30a8c0187c9b2b20
Jo <jdbass@users.noreply.github.com>
2024-04-17 13:56:13 -0400

    improve x-allow secret handling

--- tofu/modules/services/frontend/main.tf
+++ tofu/modules/services/frontend/main.tf
@@ -235,7 +235,7 @@ resource "aws_cloudfront_function" "add_index" {
         request.uri += 'index.html';
     } 
     // Check whether the URI is missing a file extension.
-    else if (uri == '') {
+    else if (!uri.includes('.')) {
         request.uri += '/index.html';
     }
 
