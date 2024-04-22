cb1216e7094f265b0e3badf91737a0e02196ca24
Jo <jdbass@users.noreply.github.com>
2024-04-22 12:40:20 -0400

    add smtp outbound

--- tofu/modules/services/backend-infra/main.tf
+++ tofu/modules/services/backend-infra/main.tf
@@ -255,3 +255,21 @@ resource "aws_vpc_security_group_egress_rule" "allow_smtp_to_all" {
   ip_protocol       = "tcp"
   cidr_ipv4         = "0.0.0.0/0"
 }
+
+resource "aws_vpc_security_group_egress_rule" "allow_smtp_to_all" {
+  security_group_id = aws_security_group.backend.id
+  description       = "smtp to internet"
+  from_port         = 587
+  to_port           = 587
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_smtp_to_all" {
+  security_group_id = aws_security_group.backend.id
+  description       = "smtp to internet"
+  from_port         = 587
+  to_port           = 587
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+}
