3d87c8efc1252d497f5feae4b2d962f2a0476780
Melissa Autumn <melissa@thunderbird.net>
2024-05-23 15:06:01 -0700

    Add invite factory

---
+++ backend/test/factory/invite_factory.py
@@ -0,0 +1,19 @@
+import pytest
+from faker import Faker
+from appointment.database import models
+from defines import FAKER_RANDOM_VALUE, factory_has_value
+
+
+@pytest.fixture
+def make_invite(with_db):
+    fake = Faker()
+
+    def _make_invite(subscriber_id=None, code=FAKER_RANDOM_VALUE, status=models.InviteStatus.active) -> models.Invite:
+        with with_db() as db:
+            invite = models.Invite(subscriber_id=subscriber_id, status=status, code=code if factory_has_value(code) else fake.uuid4())
+            db.add(invite)
+            db.commit()
+            db.refresh(invite)
+            return invite
+
+    return _make_invite
