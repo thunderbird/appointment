6f8edc3eda97d7a56f3f1bd552d5c9161d25efc3
Andreas Müller <mail@devmount.de>
2024-06-21 10:19:58 +0200

    🔨 Type alert store

--- frontend/src/stores/alert-store.ts
+++ frontend/src/stores/alert-store.ts
@@ -14,25 +14,24 @@ export const useSiteNotificationStore = defineStore('siteNotification', () => {
 
   /**
    * Check a given id if it is currently locked
-   * @param {string} checkId notification id to check agains current id
-   * @returns {boolean}
+   * @param checkId notification id to check agains current id
    */
-  const isSame = (checkId) => id.value === checkId;
+  const isSame = (checkId: string) => id.value === checkId;
 
   /**
    * Lock the given id
-   * @param {string} lockId notification id to lock in
+   * @param lockId notification id to lock in
    */
-  const lock = (lockId) => { id.value = lockId; };
+  const lock = (lockId: string) => { id.value = lockId; };
 
   /**
    * Make a notification with given configuration appear
-   * @param {string} showId notification identifier
-   * @param {string} showTitle notification title
-   * @param {string} showMessage notification message
-   * @param {string} showActionUrl target url if notification should be a link
+   * @param showId notification identifier
+   * @param showTitle notification title
+   * @param showMessage notification message
+   * @param showActionUrl target url if notification should be a link
    */
-  const show = (showId, showTitle, showMessage, showActionUrl) => {
+  const show = (showId: string, showTitle: string, showMessage: string, showActionUrl: string) => {
     isVisible.value = true;
     id.value = showId;
     title.value = showTitle;
