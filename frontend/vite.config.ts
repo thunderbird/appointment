63a48c5ee13f952fde7aca8cc1cd5ca145924647
Andreas Müller <mail@devmount.de>
2024-07-10 09:35:30 +0200

    ➕ Add configuration to fix HMR

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -26,6 +26,13 @@ export default defineConfig({
 
   server: {
     host: '0.0.0.0',
+    hmr: {
+      clientPort: 8080,
+    },
+    port: 8080,
+    watch: {
+      usePolling: true,
+    },
   },
 
   build: {
