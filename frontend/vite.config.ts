e303c345f1c3a6151d7708891a13b26465084e63
Andreas Müller <mail@devmount.de>
2024-07-15 20:00:00 +0200

    🔨 Only use necessary options

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,24 @@
 import { sentryVitePlugin } from '@sentry/vite-plugin';
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig, loadEnv, WatchOptions } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
   const env = loadEnv(mode, process.cwd());
+
+  // Build watch options
+  const watch = {
+    usePolling: env.VITE_SERVER_WATCH_POLLING === 'true'
+  } as WatchOptions;
+  if (env.VITE_SERVER_WATCH_INTERVAL) {
+    watch.interval = Number(env.VITE_SERVER_WATCH_INTERVAL);
+  }
+  if (env.VITE_SERVER_WATCH_BINARY_INTERVAL) {
+    watch.binaryInterval = Number(env.VITE_SERVER_WATCH_BINARY_INTERVAL);
+  }
 
   return {
     plugins: [
@@ -30,15 +41,7 @@ export default defineConfig(({ mode }) => {
 
     server: {
       host: '0.0.0.0',
-      hmr: {
-        clientPort: 8080,
-      },
-      port: 8080,
-      watch: {
-        usePolling: env.VITE_SERVER_WATCH_POLLING === 'true',
-        interval: 1500,
-        binaryInterval: 3000,
-      },
+      watch: watch,
     },
 
     build: {
