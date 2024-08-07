b8e84ab3ebe4c2cab5a3decff44b94ea6fa155aa
Andreas Müller <mail@devmount.de>
2024-08-07 20:15:25 +0200

    ➕ Add missing types to list pagination

--- frontend/src/elements/ListPagination.vue
+++ frontend/src/elements/ListPagination.vue
@@ -36,17 +36,17 @@ const goto = (index) => {
   emit('update', currentPage.value);
 };
 
-const isVisibleInnerPage = (p) => (currentPage.value === 0 && p === 3)
+const isVisibleInnerPage = (p: number) => (currentPage.value === 0 && p === 3)
     || ((currentPage.value === 0 || currentPage.value === 1) && p === 4)
     || (p > currentPage.value - 1 && p < currentPage.value + 3)
     || ((currentPage.value === pageCount.value - 1 || currentPage.value === pageCount.value - 2) && p === pageCount.value - 3)
     || (currentPage.value === pageCount.value - 1 && p === pageCount.value - 2)
     || p === pageCount.value;
 
-const showPageItem = (p) => pageCount.value < 6 || p === 1 || p === 2 || isVisibleInnerPage(p) || p === pageCount.value - 1;
-const showFirstEllipsis = (p) => pageCount.value >= 6 && currentPage.value > 2 && p === 2;
-const showPageItemLink = (p) => pageCount.value < 6 || p === 1 || isVisibleInnerPage(p);
-const showLastEllipsis = (p) => pageCount.value >= 6 && currentPage.value < pageCount.value - 3 && p === pageCount.value - 1;
+const showPageItem = (p: number) => pageCount.value < 6 || p === 1 || p === 2 || isVisibleInnerPage(p) || p === pageCount.value - 1;
+const showFirstEllipsis = (p: number) => pageCount.value >= 6 && currentPage.value > 2 && p === 2;
+const showPageItemLink = (p: number) => pageCount.value < 6 || p === 1 || isVisibleInnerPage(p);
+const showLastEllipsis = (p: number) => pageCount.value >= 6 && currentPage.value < pageCount.value - 3 && p === pageCount.value - 1;
 </script>
 
 <template>
