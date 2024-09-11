451702004fcd71339794136f1e1a6c0a751cb80a
Melissa Autumn <melissa@thunderbird.net>
2024-09-11 13:56:16 -0700

    Add tests for /me/appointments

--- backend/test/integration/test_appointment.py
+++ backend/test/integration/test_appointment.py
@@ -1,6 +1,6 @@
 import dateutil.parser
 
-from defines import DAY1, DAY2, DAY3, auth_headers
+from defines import DAY1, DAY2, DAY3, auth_headers, TEST_USER_ID
 
 
 class TestAppointment:
@@ -70,3 +70,44 @@ class TestAppointment:
             f'/apmt/serve/ics/{generated_appointment.slug}/{generated_appointment.slots[0].id + 1}'
         )
         assert response.status_code == 404, response.text
+
+
+class TestMyAppointments:
+    def test_appointments(self, with_client, make_appointment, make_google_calendar):
+        appointment_count = 10
+
+        calendar = make_google_calendar(subscriber_id=TEST_USER_ID)
+        appointments = [make_appointment(calendar_id=calendar.id) for _ in range(appointment_count)]
+
+        response = with_client.get('/me/appointments', headers=auth_headers)
+
+        assert response.status_code == 200, response.text
+
+        data = response.json()
+
+        assert len(data) == appointment_count
+
+        # Should be in order
+        for index, appointment in enumerate(appointments):
+            assert appointment.id == data[index]['id']
+
+    def test_dont_show_other_subscribers_appointments(self, with_client, make_basic_subscriber, make_appointment, make_google_calendar):
+        """Only show my appointments in the /me/appointments route"""
+        # The other subscriber / appointment
+        other = make_basic_subscriber()
+        other_calendar = make_google_calendar(subscriber_id=other.id)
+        other_appointment = make_appointment(calendar_id=other_calendar.id)
+
+        # My Appointment
+        calendar = make_google_calendar(subscriber_id=TEST_USER_ID)
+        appointment = make_appointment(calendar_id=calendar.id)
+
+        response = with_client.get('/me/appointments', headers=auth_headers)
+
+        assert response.status_code == 200, response.text
+
+        data = response.json()
+
+        assert len(data) == 1
+        assert other_appointment.id != data[0]['id']
+        assert appointment.id == data[0]['id']
