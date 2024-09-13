207d63d253dddd3d6d2b0e66bc3f2663cd319772
Jo <jdbass@users.noreply.github.com>
2024-09-13 16:00:28 -0400

    Reapply "#634 added redis ports to backend and cache SGs"
    This reverts commit c4343c590e9060efde13e73664b973af858c0d37.
    
    	modified:   tofu/modules/data-store/cache/main.tf
    	modified:   tofu/modules/services/backend-infra/main.tf

--- tofu/modules/data-store/cache/main.tf
+++ tofu/modules/data-store/cache/main.tf
@@ -22,13 +22,28 @@ resource "aws_security_group" "redis" {
   })
 }
 
-resource "aws_vpc_security_group_ingress_rule" "ingress" {
+resource "aws_vpc_security_group_ingress_rule" "ingress_6379" {
   for_each = toset(var.source_security_groups)
 
   security_group_id            = aws_security_group.redis.id
   description                  = "redis(6379) ingress"
   from_port                    = 6379
   to_port                      = 6379
+  ip_protocol                  = "tcp"
+  referenced_security_group_id = each.key
+
+  tags = merge(var.tags, {
+    Name = "${var.name_prefix}-redis"
+  })
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ingress_6380" {
+  for_each = toset(var.source_security_groups)
+
+  security_group_id            = aws_security_group.redis.id
+  description                  = "redis(6380) ingress"
+  from_port                    = 6380
+  to_port                      = 6380
   ip_protocol                  = "tcp"
   referenced_security_group_id = each.key
 
@@ -46,6 +61,36 @@ resource "aws_vpc_security_group_egress_rule" "egress" {
   to_port                      = 3306
   ip_protocol                  = "tcp"
   cidr_ipv4                    = each.key
+
+  tags = merge(var.tags, {
+    Name = "${var.name_prefix}-redis"
+  })
+}
+
+resource "aws_vpc_security_group_egress_rule" "egress_6379" {
+  for_each = toset(var.source_security_groups)
+
+  security_group_id            = each.key
+  description                  = "redis(6379) egress"
+  from_port                    = 6379
+  to_port                      = 6379
+  ip_protocol                  = "tcp"
+  referenced_security_group_id = aws_security_group.redis.id
+
+  tags = merge(var.tags, {
+    Name = "${var.name_prefix}-redis"
+  })
+}
+
+resource "aws_vpc_security_group_egress_rule" "egress_6380" {
+  for_each = toset(var.source_security_groups)
+
+  security_group_id            = each.key
+  description                  = "redis(6380) egress"
+  from_port                    = 6380
+  to_port                      = 6380
+  ip_protocol                  = "tcp"
+  referenced_security_group_id = aws_security_group.redis.id
 
   tags = merge(var.tags, {
     Name = "${var.name_prefix}-redis"
