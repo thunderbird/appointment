7614434ce02ca8fa0aedce9604ee8dae6a3b8995
Mel <97147377+MelissaAutumn@users.noreply.github.com>
2024-09-25 08:38:04 -0700

    Add a beta warning warning people about the fact we're still in beta. (#685)
    * Add a beta warning warning people about the fact we're still in beta.
    
    Be warned.
    
    * Updated with German localization
    
    Co-authored-by: Philipp Kewisch <607198+kewisch@users.noreply.github.com>
    
    * Add a comment on what user-activity-store does.
    
    ---------
    
    Co-authored-by: Philipp Kewisch <607198+kewisch@users.noreply.github.com>

--- frontend/src/views/ScheduleView.vue
+++ frontend/src/views/ScheduleView.vue
@@ -2,7 +2,7 @@
 import {
   BookingCalendarView,
   DateFormatStrings,
-  DEFAULT_SLOT_DURATION,
+  DEFAULT_SLOT_DURATION, Dismissibles,
   EventLocationType,
   MeetingLinkProviderType,
 } from '@/definitions';
@@ -23,10 +23,13 @@ import {
 } from '@/models';
 import ScheduleCreation from '@/components/ScheduleCreation.vue';
 import CalendarQalendar from '@/components/CalendarQalendar.vue';
+import NoticeBar from '@/tbpro/elements/NoticeBar.vue';
+import PrimaryButton from '@/tbpro/elements/PrimaryButton.vue';
 
 // stores
 import { useAppointmentStore } from '@/stores/appointment-store';
 import { useCalendarStore } from '@/stores/calendar-store';
+import { useUserActivityStore } from '@/stores/user-activity-store';
 
 const { t } = useI18n();
 const route = useRoute();
@@ -36,8 +39,10 @@ const refresh = inject(refreshKey);
 
 const appointmentStore = useAppointmentStore();
 const calendarStore = useCalendarStore();
+const userActivityStore = useUserActivityStore();
 const { pendingAppointments } = storeToRefs(appointmentStore);
 const { connectedCalendars } = storeToRefs(calendarStore);
+const { data: userActivityData } = storeToRefs(userActivityStore);
 
 // current selected date, if not in route: defaults to now
 const activeDate = ref(route.params.date ? dj(route.params.date as string) : dj());
@@ -146,9 +151,43 @@ onMounted(async () => {
   const eventsTo = dj(activeDate.value).endOf('month').format('YYYY-MM-DD');
   await getRemoteEvents(eventsFrom, eventsTo);
 });
+
+const dismiss = () => {
+  userActivityStore.dismiss(Dismissibles.BetaWarning);
+};
 </script>
 
 <template>
+  <notice-bar type="info" id="beta-warning" v-if="!userActivityData.dismissedBetaWarning">
+    <p>{{ t('notices.betaWarning.heading') }}</p>
+    <ul>
+      <li>{{ t('notices.betaWarning.list.0') }}</li>
+      <li>
+        <i18n-t keypath="notices.betaWarning.list.1">
+          <template v-slot:connectedAccounts>
+            <router-link class="underline" :to="{ path: '/settings/connectedAccounts' }" target="_blank">
+              {{ t('notices.betaWarning.linkText.connectedAccounts') }}
+            </router-link>
+          </template>
+        </i18n-t>
+      </li>
+      <li>
+        <i18n-t keypath="notices.betaWarning.list.2">
+          <template v-slot:contactUs>
+            <router-link class="underline" :to="{ name: 'contact' }" target="_blank">
+              {{ t('notices.betaWarning.linkText.contactUs') }}
+            </router-link>
+          </template>
+          <template v-slot:matrixChannel>
+            <a class="underline" href="https://matrix.to/#/#tb-services:mozilla.org" target="_blank">
+              {{ t('notices.betaWarning.linkText.matrixChannel') }}
+            </a>
+          </template>
+        </i18n-t>
+      </li>
+    </ul>
+    <primary-button class="dismiss" size="small" @click="dismiss">Dismiss</primary-button>
+  </notice-bar>
   <!-- page title area -->
   <div class="flex select-none flex-col items-start justify-between lg:flex-row">
     <div class="text-4xl font-light">{{ t('label.dashboard') }}</div>
@@ -180,3 +219,52 @@ onMounted(async () => {
     />
   </div>
 </template>
+<style scoped>
+@import '@/assets/styles/custom-media.pcss';
+
+#beta-warning {
+  position: relative;
+  /* The navbar provides margin already */
+  margin: 0 0 2rem;
+
+  :deep(.icon) {
+    top: 0.75rem;
+  }
+
+  :deep(.body) {
+    text-align: left;
+    margin-left: 0.5rem;
+    line-height: 1.5;
+
+    a {
+      text-decoration: underline;
+    }
+
+    ul {
+      list-style: circle;
+      margin-left: 1rem;
+      font-weight: 400;
+    }
+
+    .dismiss {
+      margin: 1rem auto;
+    }
+  }
+}
+
+@media (--md) {
+  #beta-warning {
+    position: relative;
+    margin: 0 1rem 2rem;
+
+    :deep(.body) {
+      .dismiss {
+        position: absolute;
+        top: 0.75rem;
+        right: 1rem;
+        margin: 0;
+      }
+    }
+  }
+}
+</style>
