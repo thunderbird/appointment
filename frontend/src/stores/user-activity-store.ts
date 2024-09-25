62921924ea8f428bbd14d091ec19709f2aa02748
Melissa Autumn <melissa@thunderbird.net>
2024-09-25 08:37:03 -0700

    Add a comment on what user-activity-store does.

--- frontend/src/stores/user-activity-store.ts
+++ frontend/src/stores/user-activity-store.ts
@@ -7,6 +7,9 @@ const initialUserActivityObject = {
   dismissedBetaWarning: false,
 } as UserActivity;
 
+/**
+ * A place to store the state of UI elements, like a dismissible message or if we want something to stay minimized.
+ */
 export const useUserActivityStore = defineStore('user-activity', () => {
   const data = useLocalStorage('tba/user-activity', initialUserActivityObject);
 
