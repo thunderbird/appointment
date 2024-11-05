4349f1d83d4a4cb4bf90a37c1f6fc7d33a4d4c34
Jo <jdbass@users.noreply.github.com>
2024-11-05 10:30:33 -0500

    convert cidr sg rules to sg ids

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -38,22 +38,91 @@ backend_alb_sg = tb_pulumi.network.SecurityGroupWithRules(
   **backend_alb_sg_opts,
   opts=pulumi.ResourceOptions(depends_on=vpc),
 )
-redis_sg_opts = resources['tb:network:SecurityGroupWithRules']['redis']
-redis_sg = tb_pulumi.network.SecurityGroupWithRules(
-  f'{project.name_prefix}-redis-sg',
+cache_sg = tb_pulumi.network.SecurityGroup(
+  f'{project.name_prefix}-cache-sg',
   project,
   vpc_id=vpc.resources['vpc'].id,
-  **redis_sg_opts,
   opts=pulumi.ResourceOptions(depends_on=vpc),
 )
-rds_sg_opts = resources['tb:network:SecurityGroupWithRules']['rds']
-rds_sg = tb_pulumi.network.SecurityGroupWithRules(
-  f'{project.name_prefix}-rds-sg',
+db_sg = tb_pulumi.network.SecurityGroup(
+  f'{project.name_prefix}-db-sg',
   project,
   vpc_id=vpc.resources['vpc'].id,
-  **rds_sg_opts,
   opts=pulumi.ResourceOptions(depends_on=vpc),
 )
+backend_from_alb_rule_opts = resources['tb:network:SecurityGroupRule']['backend_ingress_from_alb']
+backend_from_alb_rule_opts['config']['security_group_id'] = backend_sg.resources['sg'].id
+backend_from_alb_rule_opts['config']['source_security_group_id'] = backend_alb_sg.resources['sg'].id
+backend_from_alb_rule = tb_pulumi.network.SecurityGroupRule(
+  f'{project.name_prefix}-backend-from-alb',
+  project,
+  **backend_from_alb_rule_opts,
+  opts=pulumi.ResourceOptions(depends_on=[backend_sg, backend_alb_sg])
+)
+backend_to_db_rule_opts = resources['tb:network:SecurityGroupRule']['backend_egress_to_db']
+backend_to_db_rule_opts['config']['security_group_id'] = backend_sg.resources['sg'].id
+backend_to_db_rule_opts['config']['source_security_group_id'] = db_sg.resources['sg'].id
+backend_to_db_rule = tb_pulumi.network.SecurityGroupRule(
+  f'{project.name_prefix}-backend-to-db',
+  project,
+  **backend_to_db_rule_opts,
+  opts=pulumi.ResourceOptions(depends_on=[backend_sg, db_sg])
+)
+backend_to_cache_rule_opts = resources['tb:network:SecurityGroupRule']['backend_egress_to_cache']
+backend_to_cache_rule_opts['config']['security_group_id'] = backend_sg.resources['sg'].id
+backend_to_cache_rule_opts['config']['source_security_group_id'] = cache_sg.resources['sg'].id
+backend_to_cache_rule = tb_pulumi.network.SecurityGroupRule(
+  f'{project.name_prefix}-backend-to-cache',
+  project,
+  **backend_to_cache_rule_opts,
+  opts=pulumi.ResourceOptions(depends_on=[backend_sg, cache_sg])
+)
+backend_alb_to_backend_rule_opts = resources['tb:network:SecurityGroupRule']['backend_alb_egress_to_backend']
+backend_alb_to_backend_rule_opts['config']['security_group_id'] = backend_alb_sg.resources['sg'].id
+backend_alb_to_backend_rule_opts['config']['source_security_group_id'] = backend_sg.resources['sg'].id
+backend_alb_to_backend_rule = tb_pulumi.network.SecurityGroupRule(
+  f'{project.name_prefix}-backend_alb_to_backend',
+  project,
+  **backend_alb_to_backend_rule_opts,
+  opts=pulumi.ResourceOptions(depends_on=[backend_alb_sg, backend_sg])
+)
+cache_from_backend_rule_opts = resources['tb:network:SecurityGroupRule']['cache_ingress_from_backend']
+cache_from_backend_rule_opts['config']['security_group_id'] = cache_sg.resources['sg'].id
+cache_from_backend_rule_opts['config']['source_security_group_id'] = backend_sg.resources['sg'].id
+cache_from_backend_rule = tb_pulumi.network.SecurityGroupRule(
+  f'{project.name_prefix}-cache_from_backend',
+  project,
+  **cache_from_backend_rule_opts,
+  opts=pulumi.ResourceOptions(depends_on=[cache_sg, backend_sg])
+)
+cache_to_db_rule_opts = resources['tb:network:SecurityGroupRule']['cache_egress_to_db']
+cache_to_db_rule_opts['config']['security_group_id'] = cache_sg.resources['sg'].id
+cache_to_db_rule_opts['config']['source_security_group_id'] = db_sg.resources['sg'].id
+cache_to_db_rule = tb_pulumi.network.SecurityGroupRule(
+  f'{project.name_prefix}-cache_to_db',
+  project,
+  **cache_to_db_rule_opts,
+  opts=pulumi.ResourceOptions(depends_on=[cache_sg, db_sg])
+)
+db_from_backend_rule_opts = resources['tb:network:SecurityGroupRule']['db_ingress_from_backend']
+db_from_backend_rule_opts['config']['security_group_id'] = db_sg.resources['sg'].id
+db_from_backend_rule_opts['config']['source_security_group_id'] = backend_sg.resources['sg'].id
+db_from_backend_rule = tb_pulumi.network.SecurityGroupRule(
+  f'{project.name_prefix}-db_from_backend',
+  project,
+  **db_from_backend_rule_opts,
+  opts=pulumi.ResourceOptions(depends_on=[db_sg, backend_sg])
+)
+db_from_cache_rule_opts = resources['tb:network:SecurityGroupRule']['db_ingress_from_cache']
+db_from_cache_rule_opts['config']['security_group_id'] = db_sg.resources['sg'].id
+db_from_cache_rule_opts['config']['source_security_group_id'] = cache_sg.resources['sg'].id
+db_from_cache_rule = tb_pulumi.network.SecurityGroupRule(
+  f'{project.name_prefix}-db_from_cache',
+  project,
+  **db_from_cache_rule_opts,
+  opts=pulumi.ResourceOptions(depends_on=[db_sg, cache_sg])
+)
+
 #rds_opts = resources['tb:rds:RdsDatabaseGroup']['database']
 #rds = tb_pulumi.rds.RdsDatabaseGroup(
 #  f'{project.name_prefix}-rds',
