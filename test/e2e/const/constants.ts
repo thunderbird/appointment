83ebd6bb0d186e2d6b8fa8fb649eeb17faf1b4f3
rwood-moz <rwood@thunderbird.net>
2024-12-17 12:34:30 -0500

    Expand prod sanity test

--- test/e2e/const/constants.ts
+++ test/e2e/const/constants.ts
@@ -1,10 +1,15 @@
 // appointment urls
 export const APPT_PROD_URL = process.env.APPT_PROD_URL;
+export const PROD_SHARE_LINK_SHORT = process.env.APPT_PROD_SHARE_LINK_SHORT;
+export const PROD_SHARE_LINK_LONG = process.env.APPT_PROD_SHARE_LINK_LONG;
 
 // page titles
 export const APPT_PAGE_TITLE = 'Thunderbird Appointment';
 export const FXA_PAGE_TITLE = 'Mozilla accounts';
 
-// production sign-in credentials
+// production sign-in credentials and corresponding account display name
 export const PROD_LOGIN_EMAIL = process.env.APPT_PROD_LOGIN_EMAIL;
 export const PROD_LOGIN_PWORD = process.env.APPT_PROD_LOGIN_PWORD;
+
+// appointment user display name (settings => account) for above user
+export const PROD_ACCT_DISPLAY_NAME = process.env.APPT_PROD_ACCT_DISPLAY_NAME;
---
+++ test/e2e/tests/book-appointment.spec.ts
@@ -0,0 +1,37 @@
+import { test, expect } from '@playwright/test';
+import { BookingPage } from '../pages/booking-page';
+import { PROD_SHARE_LINK_SHORT, PROD_SHARE_LINK_LONG, PROD_ACCT_DISPLAY_NAME } from '../const/constants';
+
+let bookingPage: BookingPage;
+
+// verify booking page loaded successfully
+const verifyBookingPageLoaded = async () => {
+  await expect(bookingPage.titleText).toBeVisible();
+  await expect(bookingPage.titleText).toContainText(String(PROD_ACCT_DISPLAY_NAME));
+  await expect(bookingPage.invitingText).toBeVisible();
+  await expect(bookingPage.invitingText).toContainText(String(PROD_ACCT_DISPLAY_NAME));
+  // todo check some more locators
+}
+
+test.beforeEach(async ({ page }) => {
+  bookingPage = new BookingPage(page);
+});
+
+// verify we are able to book an appointment using existing user's share link
+test.describe('book an appointment', {
+  tag: '@prod-sanity'
+}, () => {
+  test('able to access booking page via short link', async ({ page }) => {
+    await bookingPage.gotoBookingPage(String(PROD_SHARE_LINK_SHORT));
+    await verifyBookingPageLoaded();
+  });
+
+  test('able to access booking page via long link', async ({ page }) => {
+    await bookingPage.gotoBookingPage(String(PROD_SHARE_LINK_LONG));
+    await verifyBookingPageLoaded();
+  });
+
+  test('able to request a booking', async ({ page }) => {
+    await expect(true).toBeTruthy();
+  });
+});
