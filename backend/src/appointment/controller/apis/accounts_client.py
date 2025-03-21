1acf55d30d52b5ea421125a96f1737f9effdf432
Melissa Autumn <melissa@thunderbird.net>
2025-03-21 14:23:12 -0700

    Add instructions on how to setup accounts, and add the allow-list route.

--- backend/src/appointment/controller/apis/accounts_client.py
+++ backend/src/appointment/controller/apis/accounts_client.py
@@ -3,6 +3,8 @@ import os
 
 import requests
 
+from appointment.database import repo
+
 
 class AccountsClient:
     subscriber_id: int | None = None
@@ -21,20 +23,37 @@ class AccountsClient:
         self.token = token
 
     def is_in_allow_list(self, db, email: str):
-        """FIXME: Not Impl"""
-        return True
+        # Allow existing subscribers to login even if they're not on an allow-list
+        subscriber = repo.subscriber.get_by_email(db, email)
+        if subscriber:
+            return True
+
+        try:
+            response = requests.post(
+                url=f'{self.accounts_url}/api/v1/auth/is-in-allow-list/',
+                json={'email': email, 'secret': self.client_secret},
+                headers={
+                    'Accept': 'application/json',
+                },
+            )
+
+            response.raise_for_status()
+
+            data = response.json()
+            return data.get('result', False)
+        except requests.HTTPError as e:
+            logging.error(f'Could not perform allow list check: {e.response.status_code} - {e.response.text}')
+
+        return False
 
     def get_redirect_url(self, state):
         try:
             response = requests.post(
                 url=f'{self.accounts_url}/api/v1/auth/get-login/',
-                json={
-                    'secret': self.client_secret,
-                    'state': state
-                },
+                json={'secret': self.client_secret, 'state': state},
                 headers={
                     'Accept': 'application/json',
-                }
+                },
             )
 
             response.raise_for_status()
@@ -52,12 +71,10 @@ class AccountsClient:
         try:
             response = requests.post(
                 url=f'{self.accounts_url}/api/v1/token/refresh/',
-                json={
-                    'refresh': token
-                },
+                json={'refresh': token},
                 headers={
                     'Accept': 'application/json',
-                }
+                },
             )
 
             response.raise_for_status()
@@ -75,22 +92,17 @@ class AccountsClient:
         try:
             response = requests.post(
                 url=f'{self.accounts_url}/api/v1/auth/get-profile/',
-                headers={
-                    'Accept': 'application/json',
-                    'Authorization': f'Bearer {token}'
-                }
+                headers={'Accept': 'application/json', 'Authorization': f'Bearer {token}'},
             )
 
             response.raise_for_status()
 
             profile_response = response.json()
-            print(">", profile_response)
+            print('>', profile_response)
             return profile_response
         except requests.HTTPError as e:
             # Not great, but we can still continue along..
-            logging.error(
-                f'Could not retrieve profile, error occurred: {e.response.status_code} - {e.response.text}'
-            )
+            logging.error(f'Could not retrieve profile, error occurred: {e.response.status_code} - {e.response.text}')
             raise e
 
     def logout(self):
@@ -98,10 +110,7 @@ class AccountsClient:
         try:
             response = requests.post(
                 url=f'{self.accounts_url}/api/v1/auth/logout/',
-                headers={
-                    'Accept': 'application/json',
-                    'Authorization': f'Bearer {self.token}'
-                },
+                headers={'Accept': 'application/json', 'Authorization': f'Bearer {self.token}'},
             )
 
             response.raise_for_status()
@@ -110,7 +119,5 @@ class AccountsClient:
             return logout_response.get('success')
         except requests.HTTPError as e:
             # Not great, but we can still continue along..
-            logging.error(
-                f'Could not log out the user, error occurred: {e.response.status_code} - {e.response.text}'
-            )
+            logging.error(f'Could not log out the user, error occurred: {e.response.status_code} - {e.response.text}')
             raise e
