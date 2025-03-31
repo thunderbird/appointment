2933644f814e58790e8854513430fb49d516c634
Melissa Autumn <melissa@thunderbird.net>
2025-03-31 08:54:21 -0700

    Push up some debugging code for stage

--- backend/src/appointment/dependencies/auth.py
+++ backend/src/appointment/dependencies/auth.py
@@ -16,6 +16,7 @@ from ..database import repo, models
 from ..defines import AuthScheme, REDIS_USER_SESSION_PROFILE_KEY
 from ..dependencies.database import get_db
 from ..exceptions import validation
+from ..exceptions.misc import UnexpectedBehaviourWarning
 from ..exceptions.validation import InvalidTokenException, InvalidPermissionLevelException
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token', auto_error=False)
@@ -37,7 +38,19 @@ def get_user_from_accounts_session(request, db):
     return repo.external_connection.get_subscriber_by_accounts_uuid(db, user_profile.get('uuid'))
 
 
+def flag_code(reason, debug_obj):
+    # Raise and catch the unexpected behaviour warning so we can get proper stacktrace in sentry...
+    try:
+        sentry_sdk.set_extra('debug_object', debug_obj)
+        raise UnexpectedBehaviourWarning(message=reason, info=debug_obj)
+    except UnexpectedBehaviourWarning as ex:
+        sentry_sdk.capture_exception(ex)
+
+
 def get_user_from_token(db, token: str, require_jti=False):
+    sub = None
+    iat = None
+    jti = None
     try:
         payload = jwt.decode(token, os.getenv('JWT_SECRET'), algorithms=[os.getenv('JWT_ALGO')])
         sub = payload.get('sub')
@@ -45,7 +58,11 @@ def get_user_from_token(db, token: str, require_jti=False):
         jti = payload.get('jti')
         if sub is None:
             raise InvalidTokenException()
-    except jwt.exceptions.InvalidTokenError:
+    except jwt.exceptions.InvalidTokenError as ex:
+        flag_code(
+            'Invalid Token Error - Exception',
+            debug_obj={'exception': ex, 'subscriber': sub, 'issued_at': iat, 'token_id': jti},
+        )
         raise InvalidTokenException()
 
     sub_id = sub.replace('uid-', '')
@@ -53,6 +70,7 @@ def get_user_from_token(db, token: str, require_jti=False):
 
     # Check this first as any doesn't short-circuit
     if subscriber is None:
+        flag_code('Invalid Token Error - No Sub', debug_obj={'subscriber': sub, 'issued_at': iat, 'token_id': jti})
         raise InvalidTokenException()
 
     # Token has been expired by us - temp measure to avoid spinning a refresh system, or a deny list for this issue
@@ -66,6 +84,17 @@ def get_user_from_token(db, token: str, require_jti=False):
             require_jti and not jti,
         ]
     ):
+        flag_code(
+            'Invalid Token Error - Expired',
+            debug_obj={
+                'minimum_issued_at': subscriber.minimum_valid_iat_time,
+                'minimum_issued_at_timestamp': int(subscriber.minimum_valid_iat_time.timestamp()),
+                'subscriber': sub,
+                'issued_at': iat,
+                'require_token_id': require_jti,
+                'token_id': jti,
+            },
+        )
         raise InvalidTokenException()
 
     # If we're a one-time token, set the minimum valid iat time to now!
