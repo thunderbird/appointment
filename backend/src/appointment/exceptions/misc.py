fccc166eeee2f3b3b9803b5b6b5c04eff0025729
Melissa Autumn <melissa@thunderbird.net>
2025-04-01 11:44:55 -0700

    Hopefully fix context for flag_code calls

--- backend/src/appointment/exceptions/misc.py
+++ backend/src/appointment/exceptions/misc.py
@@ -1,4 +1,5 @@
 class UnexpectedBehaviourWarning(RuntimeWarning):
-    def __init__(self, message: str, info: dict):
+    def __init__(self, message: str, info: dict, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.message = message
         self.info = info
