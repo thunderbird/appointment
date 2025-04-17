068795a4b250dfc6e0d9f7cbb15e91970acf9e38
Andreas Müller <mail@devmount.de>
2025-04-18 00:54:37 +0200

    💚 Migrate settings input and buttons to tbpro components

---
+++ frontend/src/tbpro/elements/DangerButton.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import BaseButton from '@/tbpro/elements/BaseButton.vue';
+</script>
+
+<template>
+  <base-button type="danger">
+    <template v-for="(_, name) in $slots" v-slot:[name]="slotData">
+      <slot :name="name" v-bind="slotData" />
+    </template>
+  </base-button>
+</template>
