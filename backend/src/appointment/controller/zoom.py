e48d9ab390d2925d37bb9684ea6f812c9f60eb35
Davi Nakano <davinakanoca@gmail.com>
2025-06-21 15:00:56 -0600

    upon zoom connection, update schedules meeting_link_provider to zoom

--- backend/src/appointment/controller/zoom.py
+++ backend/src/appointment/controller/zoom.py
@@ -4,6 +4,16 @@ from appointment.database import repo, models
 from appointment.database.models import ExternalConnectionType
 
 
+def update_schedules_meeting_link_provider(db: Session, subscriber_id: int) -> bool:
+    """Updates existing schedules' meeling link provider to be zoom"""
+    schedules = repo.schedule.get_by_subscriber(db, subscriber_id)
+    for schedule in schedules:
+        schedule.meeting_link_provider = models.MeetingLinkProviderType.zoom
+        db.add(schedule)
+    db.commit()
+    return True
+
+
 def disconnect(db: Session, subscriber_id: int, type_id: str) -> bool:
     """Disconnects a zoom external connection from a given subscriber id and zoom type id"""
     repo.external_connection.delete_by_type(db, subscriber_id, ExternalConnectionType.zoom, type_id)
