97439a62bdf1ed5f47302dcaccfece202e1e7270
Ryan Jung <rjung@thunderbird.net>
2025-06-27 13:29:54 -0600

    Build out CloudFront distribution

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -11,6 +11,13 @@ import tb_pulumi.network
 import tb_pulumi.rds
 import tb_pulumi.secrets
 
+from tb_pulumi.constants import (
+    CLOUDFRONT_CACHE_POLICY_ID_DISABLED,
+    CLOUDFRONT_CACHE_POLICY_ID_OPTIMIZED,
+    CLOUDFRONT_ORIGIN_REQUEST_POLICY_ID_ALLVIEWER,
+)
+
+CLOUDFRONT_REWRITE_CODE_FILE = 'cloudfront-rewrite.js'
 MSG_LB_MATCHING_CONTAINER = 'In this stack, container security groups must have matching load balancer security groups.'
 MSG_LB_MATCHING_CLUSTER = 'In this stack, Fargate clusters must have matching load balancer security groups.'
 MSG_CONTAINER_MATCHING_CLUSTER = 'In this stack, Fargate clusters must have matching container security groups.'
@@ -136,9 +143,93 @@ for service, opts in resources['tb:fargate:FargateClusterWithLogging'].items():
         **opts,
     )
 
-# CloudFrontS3Service
+# Manage the CloudFront rewrite function; the code is managed in cloudfront-rewrite.js
+rewrite_code = None
+try:
+    with open(CLOUDFRONT_REWRITE_CODE_FILE, 'r') as fh:
+        rewrite_code = fh.read()
+except IOError:
+    pulumi.error(f'Could not read file {CLOUDFRONT_REWRITE_CODE_FILE}')
 
 # CF Function to rewrite requests
+cf_func = aws.cloudfront.Function(
+    f'{project.name_prefix}-func-rewrite',
+    code=rewrite_code,
+    comment='Rewrites inbound requests to direct them to the Appointment backend API',
+    name=f'{project.name_prefix}-rewrite',
+    publish=True,
+    runtime='cloudfront-js-2.0',
+)
+project.resources['cf_rewrite_function'] = cf_func
+
+# Craft all the things we need for our CloudFront Distribution
+backend_domain_name = fargate_clusters['backend'].resources['fargate_service_alb'].resources['albs']['backend'].dns_name
+backend_origin_id = f'{project.name_prefix}-backend'
+frontend_opts = resources.get('tb:cloudfront:CloudFrontS3Service', {}).get('frontend', {})
+default_cache_behavior = {
+        'allowed_methods': ['GET', 'HEAD'],
+        'cached_methods': ['GET', 'HEAD'],
+        'target_origin_id': f's3-{frontend_opts["service_bucket_name"]}',
+        'cache_policy_id': CLOUDFRONT_CACHE_POLICY_ID_OPTIMIZED,
+        'function_associations': [{'event_type': 'viewer-request', 'function_arn': cf_func.arn}],
+        'viewer_protocol_policy': 'redirect-to-https',
+    }
+ordered_cache_behaviors = [
+            {
+                'path_pattern': '/api/*',
+                'allowed_methods': ['DELETE', 'GET', 'HEAD', 'OPTIONS', 'POST', 'PUT', 'PATCH'],
+                'cached_methods': ['GET', 'HEAD', 'OPTIONS'],
+                'target_origin_id': backend_origin_id,
+                'cache_policy_id': CLOUDFRONT_CACHE_POLICY_ID_DISABLED,
+                'origin_request_policy_id': CLOUDFRONT_ORIGIN_REQUEST_POLICY_ID_ALLVIEWER,
+                'function_associations': [{
+                    'event_type': 'viewer-request',
+                    'function_arn': cf_func.arn,
+                }],
+                'viewer_protocol_policy': 'redirect-to-https',
+            }, {
+                'path_pattern': '/fxa',
+                'allowed_methods': ['DELETE', 'GET', 'HEAD', 'OPTIONS', 'POST', 'PUT', 'PATCH'],
+                'cached_methods': ['GET', 'HEAD', 'OPTIONS'],
+                'target_origin_id': backend_origin_id,
+                'cache_policy_id': CLOUDFRONT_CACHE_POLICY_ID_DISABLED,
+                'origin_request_policy_id': CLOUDFRONT_ORIGIN_REQUEST_POLICY_ID_ALLVIEWER,
+                'function_associations': [{
+                    'event_type': 'viewer-request',
+                    'function_arn': cf_func.arn,
+                }],
+                'viewer_protocol_policy': 'redirect-to-https',
+            },
+        ]
+if 'distribution' in frontend_opts:
+    frontend_opts['distribution']['default_cache_behavior'] = default_cache_behavior
+    frontend_opts['distribution']['ordered_cache_behaviors'] = ordered_cache_behaviors
+else:
+    frontend_opts['distribution'] = {
+        'default_cache_behavior': default_cache_behavior,
+        'ordered_cache_behaviors': ordered_cache_behaviors
+    }
+
+frontend = tb_pulumi.cloudfront.CloudFrontS3Service(
+    name=f'{project.name_prefix}-frontend',
+    project=project,
+    # Add the backend service as an origin; the S3 bucket is automatically added by this module
+    origins=[
+        {
+            'domain_name': backend_domain_name,
+            'origin_id': backend_origin_id,
+            'custom_origin_config': {
+                'http_port': 80,
+                'https_port': 5000,
+                'origin_protocol_policy': 'https-only',
+                'origin_ssl_protocols': ['TLSv1.2'],
+            },
+        }
+    ],
+    **frontend_opts,
+    opts=pulumi.ResourceOptions(depends_on=[cf_func]),
+)
+
 
 # Monitoring
 
