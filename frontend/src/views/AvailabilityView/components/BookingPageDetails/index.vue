73ecceeea890161ba79251c30ecc54a9d479876b
Davi Nakano <davinakanoca@gmail.com>
2025-07-31 16:49:55 -0600

    booking page details section linked to store

--- frontend/src/views/AvailabilityView/components/BookingPageDetails/index.vue
+++ frontend/src/views/AvailabilityView/components/BookingPageDetails/index.vue
@@ -1,19 +1,61 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed } from 'vue';
 import { useI18n } from 'vue-i18n';
-import { SLOT_DURATION_OPTIONS } from '@/definitions';
+import { storeToRefs } from 'pinia';
+import { MeetingLinkProviderType, SLOT_DURATION_OPTIONS } from '@/definitions';
 import { SelectOption } from '@/models';
+import { useAvailabilityStore } from '@/stores/availability-store';
 import { CheckboxInput, TextArea, TextInput } from '@thunderbirdops/services-ui';
 import RadioGroupPill from '../RadioGroupPill.vue';
 
 const { t } = useI18n();
-const autoGenerateZoomLink = ref();
-const meetingDuration = ref("15_min");
+
+const availabilityStore = useAvailabilityStore();
+const { currentState, hasZoom } = storeToRefs(availabilityStore);
+
+const pageName = computed({
+  get: () => currentState.value.name?.toString(),
+  set: (value) => {
+    availabilityStore.$patch({ currentState: { name: value } })
+  }
+})
+
+const pageDescription = computed({
+  get: () => currentState.value.details?.toString(),
+  set: (value) => {
+    availabilityStore.$patch({ currentState: { details: value } })
+  }
+})
+
+const autoGenerateZoomLink = computed({
+  get: () => currentState.value.meeting_link_provider === MeetingLinkProviderType.Zoom,
+  set: (value) => {
+    availabilityStore.$patch({
+      currentState: {
+        meeting_link_provider: value ? MeetingLinkProviderType.Zoom : MeetingLinkProviderType.None
+      }
+    })
+  }
+})
+
+const virtualMeetingLink = computed({
+  get: () => currentState.value.location_url?.toString(),
+  set: (value) => {
+    availabilityStore.$patch({ currentState: { location_url: value } })
+  }
+})
+
+const meetingDuration = computed({
+  get: () => currentState.value.slot_duration?.toString(),
+  set: (value) => {
+    availabilityStore.$patch({ currentState: { slot_duration: parseInt(value, 10) } })
+  }
+})
 
 // Appointment duration options
-const durationOptions: SelectOption[] = SLOT_DURATION_OPTIONS.map((duration) => ({
-  label: t('units.minutes', { value: duration }),
-  value: duration,
+const durationOptions: SelectOption<string>[] = SLOT_DURATION_OPTIONS.map((duration) => ({
+  label: t('units.minutesShort', { value: duration }),
+  value: duration.toString(),
 }));
 </script>
 
@@ -25,56 +67,66 @@ export default {
 
 <template>
   <header>
-    <h2>Booking Page Details</h2>
+    <h2>{{ t('label.bookingPageDetails') }}</h2>
   </header>
 
   <div class="fields-container">
     <!-- Page name -->
-    <div>
-      <h3>{{ t('label.pageName') }}:</h3>
-      <p>General Availability</p>
-    </div>
+    <text-input
+      type="text"
+      name="pageName"
+      v-model="pageName"
+    >
+      {{ t('label.pageName') }}:
+    </text-input>
 
     <!-- Page description -->
     <text-area
       name="pageDescription"
       :placeholder="t('label.enterDescriptionToSchedulingPage')"
+      v-model="pageDescription"
     >
       {{ t('label.pageDescription') }}:
     </text-area>
 
     <!-- Auto generate Zoom link in invites -->
-     <checkbox-input
-      name="autoGenerateZoomLink"
-      :label="t('label.autogenerateZoomLinks')"
-      v-model="autoGenerateZoomLink"
-    />
+    <div class="auto-generate-link-container">
+      <checkbox-input
+        name="autoGenerateZoomLink"
+        :label="t('label.autogenerateZoomLinks')"
+        v-model="autoGenerateZoomLink"
+        :disabled="!hasZoom"
+      />
+      <i18n-t v-if="!hasZoom" keypath="text.generateZoomMeetingHelpDisabled.text" tag="span" scope="global" class="text-xs">
+        <template v-slot:link>
+          <router-link class="underline" to="settings/connectedAccounts">
+            {{ t('text.generateZoomMeetingHelpDisabled.link') }}
+          </router-link>
+        </template>
+      </i18n-t>
+    </div>
 
     <text-input
       type="text"
       name="virtualMeetingLink"
+      v-model="virtualMeetingLink"
     >
       {{ t('label.virtualMeetingLink') }}:
     </text-input>
 
-    <text-input
+    <!-- TBD -->
+    <!-- <text-input
       type="text"
       name="virtualMeetingDetails"
     >
       {{ t('label.virtualMeetingDetails') }}:
-    </text-input>
+    </text-input> -->
 
     <radio-group-pill
       v-model="meetingDuration"
       name="meetingDuration"
       :legend="t('label.meetingDuration')"
-      :options="[
-        { label: '15 Min', value: '15_min' },
-        { label: '30 Min', value: '30_min' },
-        { label: '60 Min', value: '60_min' },
-        { label: '75 Min', value: '75_min' },
-        { label: '90 Min', value: '90_min' },
-      ]"
+      :options="durationOptions"
     />
   </div>
 </template>
@@ -97,5 +149,11 @@ h3 {
   display: flex;
   flex-direction: column;
   gap: 1.5rem;
+}
+
+.auto-generate-link-container {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
 }
 </style>
