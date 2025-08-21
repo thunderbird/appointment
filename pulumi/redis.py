8d865f74be18bc64708c3984b2e930a8fbb80096
Ryan Jung <rjung@thunderbird.net>
2025-08-21 10:09:33 -0600

    Add tags on Redis instances

--- pulumi/redis.py
+++ pulumi/redis.py
@@ -1,3 +1,4 @@
+import pulumi
 import pulumi_aws as aws
 import pulumi_cloudflare as cloudflare
 
@@ -36,11 +37,14 @@ def redis_cache(
         f'{project.name_prefix}-cache-backend',
         security_group_ids=[security_group.resources.get('sg').id],
         subnet_ids=[subnet.id for subnet in vpc.resources.get('subnets', {})],
+        tags=project.common_tags,
         **resources.get('aws:elasticache:ServerlessCache', {}).get('backend', {}),
+        opts=pulumi.ResourceOptions(depends_on=[vpc]),
     )
+    project.resources['backend_cache'] = backend_cache
 
     backend_cache_primary_endpoint = backend_cache.endpoints.apply(lambda endpoints: endpoints[0]['address'])
-    cache_dns = cloudflare.DnsRecord(
+    backend_cache_dns = cloudflare.DnsRecord(
         f'{project.name_prefix}-dns-redis',
         name=resources.get('domains', {}).get('redis', None),
         ttl=60,
@@ -49,5 +53,6 @@ def redis_cache(
         content=backend_cache_primary_endpoint,
         proxied=False,
     )
+    project.resources['backend_cache_dns'] = backend_cache_dns
 
-    return (backend_cache, cache_dns)
+    return (backend_cache, backend_cache_dns)
