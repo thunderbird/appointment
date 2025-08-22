dd44710804114ed1b89dea30a0b62fb45f522368
Davi Nakano <davinakanoca@gmail.com>
2025-08-21 18:31:35 -0600

    remove unused computed import

--- frontend/src/components/NavBarMobile.vue
+++ frontend/src/components/NavBarMobile.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { useUserStore } from '@/stores/user-store';
 import ArtLogo from '@/elements/arts/ArtLogo.vue';
