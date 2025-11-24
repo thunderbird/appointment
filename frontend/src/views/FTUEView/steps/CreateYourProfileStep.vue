6a62fc076675c265f06199300d4696ddbca18b98
Davi Nakano <davinakanoca@gmail.com>
2025-11-24 14:34:49 -0600

    Implement CreateYourProfileStep

--- frontend/src/views/FTUEView/steps/CreateYourProfileStep.vue
+++ frontend/src/views/FTUEView/steps/CreateYourProfileStep.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import { inject, ref } from 'vue';
+import { inject, ref, useTemplateRef } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { PhArrowRight } from '@phosphor-icons/vue';
 import { BaseButton, SelectInput, TextInput } from '@thunderbirdops/services-ui';
-import { callKey, dayjsKey, shortUrlKey } from '@/keys';
-import { createUserStore } from '@/stores/user-store';
+import { dayjsKey, shortUrlKey } from '@/keys';
+import { useUserStore } from '@/stores/user-store';
 import { useFTUEStore } from '@/stores/ftue-store';
 import { FtueStep } from '@/definitions';
 
@@ -13,13 +13,17 @@ import StepTitle from '../components/StepTitle.vue';
 const { t } = useI18n();
 
 const shortUrl = inject(shortUrlKey);
-const call = inject(callKey);
 const dj = inject(dayjsKey);
 
-const user = createUserStore(call);
+const user = useUserStore();
 const ftueStore = useFTUEStore();
 
+const fullName = ref(null);
+const username = ref(null);
 const timezone = ref(user.data.settings.timezone ?? dj.tz.guess());
+const isLoading = ref(false);
+const errorMessage = ref(null);
+const formRef = useTemplateRef('formRef');
 
 // @ts-expect-error ignore type err
 // See https://github.com/microsoft/TypeScript/issues/49231
@@ -31,18 +35,44 @@ const timezoneOptions = Intl.supportedValuesOf('timeZone').map((timezone: string
 const quickLink = shortUrl.substring(shortUrl.indexOf('//')+2) + '/';
 
 const onSubmit = async () => {
-  await ftueStore.moveToStep(FtueStep.ConnectCalendars);
+  isLoading.value = true;
+
+  try {
+    const payload = {
+      name: fullName.value,
+      username: username.value,
+      timezone: timezone.value,
+    }
+
+    if (!formRef.value.checkValidity()) {
+      return;
+    }
+
+    const response = await user.updateUser(payload);
+
+    if (response.error) {
+      errorMessage.value = response.error;
+      return;
+    }
+
+    await ftueStore.moveToStep(FtueStep.ConnectCalendars);
+  } catch (error) {
+    errorMessage.value = error.message;
+  } finally {
+    isLoading.value = false;
+  }
 };
 </script>
 
 <template>
   <step-title :title="t('ftue.createYourProfile')" />
 
-  <form @submit.prevent @keyup.enter="onSubmit">
+  <form ref="formRef" @submit.prevent @keyup.enter="onSubmit">
     <text-input
       name="fullName"
       placeholder="Giles Bernam"
       required
+      v-model="fullName"
     >
       {{ t('ftue.fullName') }}
     </text-input>
@@ -52,6 +82,7 @@ const onSubmit = async () => {
       placeholder="Giles"
       required
       :outer-prefix="quickLink"
+      v-model="username"
     >
       {{ t('ftue.urlUsername') }}
     </text-input>
@@ -65,8 +96,8 @@ const onSubmit = async () => {
       {{ t('label.timeZone') }}
     </select-input>
 
-    <base-button type="brand" @click="onSubmit">
-      {{ t('ftue.createAccount') }}
+    <base-button type="brand" @click="onSubmit" :disabled="isLoading">
+      {{ isLoading ? t('label.loading') : t('ftue.createAccount') }}
 
       <template #iconRight>
         <ph-arrow-right />
