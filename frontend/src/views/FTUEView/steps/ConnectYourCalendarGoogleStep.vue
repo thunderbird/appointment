b43f29d477b948bd33b4132885c0a6e27ead4d18
Davi Nakano <davinakanoca@gmail.com>
2025-11-24 15:02:01 -0600

    Implement CalendarGoogleStep

--- frontend/src/views/FTUEView/steps/ConnectYourCalendarGoogleStep.vue
+++ frontend/src/views/FTUEView/steps/ConnectYourCalendarGoogleStep.vue
@@ -1,13 +1,97 @@
 <script setup lang="ts">
+import { ref, onMounted, inject } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import { storeToRefs } from 'pinia';
 import { useI18n } from 'vue-i18n';
 import { PrimaryButton, LinkButton } from '@thunderbirdops/services-ui';
 import { useFTUEStore } from '@/stores/ftue-store';
+import { useCalendarStore } from '@/stores/calendar-store';
+import { useUserStore } from '@/stores/user-store';
+import { useExternalConnectionsStore } from '@/stores/external-connections-store';
+import { ExternalConnectionProviders, FtueStep } from '@/definitions';
+import { callKey } from '@/keys';
+import { BooleanResponse, Exception, ExceptionDetail } from '@/models';
 
 import GradientCheckCircle from '../components/GradientCheckCircle.vue';
 import StepTitle from '../components/StepTitle.vue';
 
+const accountDashboardUrl = import.meta.env.VITE_TB_ACCOUNT_DASHBOARD_URL;
+const initFlowKey = 'tba/startedCalConnect';
+
+const call = inject(callKey);
+
 const { t } = useI18n();
+const route = useRoute();
+const router = useRouter();
+const user = useUserStore();
 const ftueStore = useFTUEStore();
+const calendarStore = useCalendarStore();
+const externalConnectionStore = useExternalConnectionsStore();
+
+const { calendars } = storeToRefs(calendarStore);
+
+const isLoading = ref(false);
+const errorMessage = ref(null);
+
+const onContinueButtonClick = async () => {
+  isLoading.value = true;
+
+  try {
+    // Create key so we can move to the next page after we come back
+    localStorage?.setItem(initFlowKey, 'true');
+    await calendarStore.connectGoogleCalendar(user.data.email);
+  } catch (error) {
+    errorMessage.value = error ? error.message : t('error.somethingWentWrong');
+  } finally {
+    isLoading.value = false;
+  }
+};
+
+onMounted(async () => {
+  // First, let's check if we have any calendars
+  await calendarStore.fetch(true);
+
+  // Are we coming back from the Google OAuth flow?
+  const hasFlowKey = localStorage?.getItem(initFlowKey);
+
+  const noCalendarsError = hasFlowKey && calendars.value.length === 0;
+
+  // Error occurred during flow
+  if (route.query.error || noCalendarsError) {
+    localStorage?.removeItem(initFlowKey);
+
+    if (noCalendarsError) {
+      errorMessage.value = t('error.externalAccountHasNoCalendars', { external: 'Google' });
+
+      // Also remove the google calendar
+      if (externalConnectionStore.google.length > 0) {
+        await externalConnectionStore.disconnect(ExternalConnectionProviders.Google);
+      }
+    } else {
+      errorMessage.value = route.query.error;
+    }
+
+    await router.replace(route.path);
+  }
+
+  if (localStorage?.getItem(initFlowKey)) {
+    localStorage?.removeItem(initFlowKey);
+
+    const { data, error }: BooleanResponse = await call('google/ftue-status').get().json();
+
+    // Did they hit back?
+    if (error?.value) {
+      errorMessage.value = {
+        title: ((data.value as Exception)?.detail as ExceptionDetail)?.message,
+        details: null,
+      };
+      return;
+    }
+
+    // We are all good, move to the next step
+    await ftueStore.moveToStep(FtueStep.CreateBookingPage);
+  }
+});
 </script>
 
 <template>
@@ -46,10 +130,12 @@ const ftueStore = useFTUEStore();
   </i18n-t>
 
   <div class="buttons-container">
-    <link-button :title="t('label.cancel')" @click="ftueStore.previousStep()">
-      {{ t('label.cancel') }}
+    <link-button :title="t('label.cancel')">
+      <a :href="accountDashboardUrl">
+        {{ t('label.cancel') }}
+      </a>
     </link-button>
-    <primary-button :title="t('label.continue')" @click="ftueStore.nextStep()">
+    <primary-button :title="t('label.continue')" @click="onContinueButtonClick">
       {{ t('label.continue') }}
     </primary-button>
   </div>
