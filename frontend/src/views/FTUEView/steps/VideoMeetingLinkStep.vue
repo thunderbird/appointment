5bf53e71e0a518311ecce53e7122dfa80d5bd19f
Davi Nakano <davinakanoca@gmail.com>
2025-11-25 12:06:39 -0600

    Implement VideoMeetingLinkStep

--- frontend/src/views/FTUEView/steps/VideoMeetingLinkStep.vue
+++ frontend/src/views/FTUEView/steps/VideoMeetingLinkStep.vue
@@ -1,18 +1,92 @@
 <script setup lang="ts">
+import { ref, inject, onMounted } from 'vue';
 import { useI18n } from 'vue-i18n';
-import { TextInput, PrimaryButton, LinkButton } from '@thunderbirdops/services-ui';
+import { TextInput, PrimaryButton, LinkButton, NoticeBar, NoticeBarTypes } from '@thunderbirdops/services-ui';
 import { useFTUEStore } from '@/stores/ftue-store';
+import { useScheduleStore } from '@/stores/schedule-store';
 import zoomLogo from '@/assets/images/zoom-icon.png';
+import { FtueStep } from '@/definitions';
+import { callKey } from '@/keys';
+import { AuthUrlResponse, AuthUrl, BooleanResponse, Exception, ExceptionDetail } from '@/models';
 
 import StepTitle from '../components/StepTitle.vue';
 import ProviderCardButton from '../components/ProviderCardButton.vue';
 
+const accountDashboardUrl = import.meta.env.VITE_TB_ACCOUNT_DASHBOARD_URL;
+const initFlowKey = 'tba/startedMeetingConnect';
+
+const call = inject(callKey);
+
 const { t } = useI18n();
 const ftueStore = useFTUEStore();
+const scheduleStore = useScheduleStore();
+
+const customMeetingLink = ref(scheduleStore.firstSchedule?.location_url ?? '');
+const isLoading = ref(false);
+const errorMessage = ref(null);
+
+const onZoomButtonClick = async () => {
+  localStorage?.setItem(initFlowKey, 'true');
+  isLoading.value = true;
+  const { data }: AuthUrlResponse = await call('zoom/auth').get().json();
+
+  // Ship them to the auth link
+  window.location.href = (data.value as AuthUrl).url;
+};
+
+const onContinueButtonClick = async () => {
+  isLoading.value = true;
+
+  try {
+    const data = await scheduleStore.updateSchedule(scheduleStore.firstSchedule.id, {
+      ...scheduleStore.firstSchedule,
+      location_url: customMeetingLink.value,
+    });
+
+    if (Object.prototype.hasOwnProperty.call(data, 'error')) {
+      errorMessage.value = (data as unknown as Error)?.message ?? t('error.somethingWentWrong');
+      return;
+    }
+
+    ftueStore.clearMessages();
+    await ftueStore.moveToStep(FtueStep.SetupComplete);
+  } catch (error) {
+    errorMessage.value = error ? error.message : t('error.somethingWentWrong');
+  } finally {
+    isLoading.value = false;
+  }
+}
+
+const onSkipButtonClick = async () => {
+  ftueStore.clearMessages();
+  await ftueStore.moveToStep(FtueStep.SetupComplete);
+};
+
+onMounted(async () => {
+  const isBackFromConnectFlow = localStorage?.getItem(initFlowKey);
+  localStorage?.removeItem(initFlowKey);
+
+  if (isBackFromConnectFlow) {
+    const { data, error }: BooleanResponse = await call('zoom/ftue-status').get().json();
+
+    // Did they hit back?
+    if (error?.value) {
+      errorMessage.value = ((data.value as Exception)?.detail as ExceptionDetail)?.message;
+      return;
+    }
+
+    ftueStore.clearMessages();
+    await ftueStore.moveToStep(FtueStep.SetupComplete);
+  }
+});
 </script>
 
 <template>
   <step-title :title="t('ftue.videoMeetingLink')" />
+
+  <notice-bar :type="NoticeBarTypes.Critical" v-if="errorMessage" class="notice-bar">
+    {{ errorMessage }}
+  </notice-bar>
 
   <div class="video-meeting-link-container">
     <provider-card-button
@@ -20,6 +94,7 @@ const ftueStore = useFTUEStore();
       :description="t('ftue.connectWithZoomInfo')"
       :iconSrc="zoomLogo"
       :iconAlt="t('ftue.zoomIcon')"
+      @click="onZoomButtonClick()"
     />
   </div>
 
@@ -29,16 +104,19 @@ const ftueStore = useFTUEStore();
   <text-input
     name="customVideoMeetingLink"
     :placeholder="t('ftue.customVideoMeetingLinkPlaceholder')"
+    v-model="customMeetingLink"
   />
 
   <div class="buttons-container">
-    <link-button :title="t('label.cancel')" @click="ftueStore.previousStep()">
-      {{ t('label.cancel') }}
+    <link-button :title="t('label.cancel')" :disabled="isLoading">
+      <a :href="accountDashboardUrl">
+        {{ t('label.cancel') }}
+      </a>
     </link-button>
-    <link-button :title="t('ftue.skipThisStep')">
+    <link-button :title="t('ftue.skipThisStep')" @click="onSkipButtonClick()" :disabled="isLoading">
       {{ t('ftue.skipThisStep') }}
     </link-button>
-    <primary-button :title="t('label.continue')" @click="ftueStore.nextStep()">
+    <primary-button :title="t('label.continue')" @click="onContinueButtonClick()" :disabled="!customMeetingLink.length || isLoading">
       {{ t('label.continue') }}
     </primary-button>
   </div>
@@ -54,6 +132,10 @@ h3 {
 
 p {
   margin-block-end: 1rem;
+}
+
+.notice-bar {
+  margin-block-end: 1.5rem;
 }
 
 .video-meeting-link-container {
