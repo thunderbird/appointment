f4b1c54f7b1b9457a30555c729f9e6c59b69ee19
Davi Nakano <davinakanoca@gmail.com>
2026-01-13 13:03:30 -0600

    Settings Page: Update Preferences section UI

--- frontend/src/views/SettingsView/components/Preferences.vue
+++ frontend/src/views/SettingsView/components/Preferences.vue
@@ -3,8 +3,7 @@ import { computed, inject } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { isoWeekdaysKey } from '@/keys';
 import { ColourSchemes } from '@/definitions';
-import { BubbleSelect, SelectInput } from '@thunderbirdops/services-ui';
-import RadioGroupPill from '@/components/RadioGroupPill.vue';
+import { BubbleSelect, SegmentedControl, SelectInput } from '@thunderbirdops/services-ui';
 import { useSettingsStore } from '@/stores/settings-store';
 import { storeToRefs } from 'pinia';
 
@@ -75,7 +74,7 @@ const timeFormat = computed({
 // TODO: As long as we use Qalendar, we can only support Sunday and Monday as start of week
 const availableStartOfTheWeekOptions = computed(
   () => isoWeekdays.filter((day) => [7,1].includes(day.iso)).map((e) => ({
-    label: e.min[0],
+    label: e.short,
     value: e.iso,
   }))
 );
@@ -102,48 +101,40 @@ export default {
   </header>
 
   <div class="form-field-container">
-    <label for="theme">
-      {{ t('label.theme') }}
-    </label>
     <select-input
       name="theme"
       :options="colourSchemeOptions"
       v-model="colourScheme"
       data-testid="settings-preferences-theme-select"
-    />
+    >
+      {{ t('label.theme') }}
+    </select-input>
 
-    <label for="language">
-      {{ t('label.language') }}
-    </label>
     <select-input
       name="language"
       :options="localeOptions"
       v-model="language"
       data-testid="settings-preferences-language-select"
-    />
+    >
+      {{ t('label.language') }}
+    </select-input>
 
-    <label for="default-time-zone">
-      {{ t('label.defaultTimeZone') }}
-    </label>
     <select-input
       name="default-time-zone"
       :options="timezoneOptions"
       v-model="defaultTimeZone"
       data-testid="settings-preferences-default-time-zone-select"
-    />
+    >
+      {{ t('label.defaultTimeZone') }}
+    </select-input>
 
-    <label class="time-format-label" for="time-format">
-      {{ t('label.timeFormat') }}
-    </label>
-    <radio-group-pill
+    <segmented-control
       name="time-format"
       v-model="timeFormat"
       :options="timeFormatOptions"
-    />
-
-    <label for="start-of-week">
-      {{ t('label.startOfWeek') }}
-    </label>
+    >
+      {{ t('label.timeFormat') }}
+    </segmented-control>
 
     <bubble-select
       name="start-of-week"
@@ -152,7 +143,9 @@ export default {
       single-selection
       :options="availableStartOfTheWeekOptions"
       :required="false"
-    />
+    >
+      {{ t('label.startOfWeek') }}
+    </bubble-select>
   </div>
 </template>
 
@@ -160,27 +153,19 @@ export default {
 @import '@/assets/styles/custom-media.pcss';
 
 header {
-  margin-block-end: 2rem;
+  margin-block-end: 1.5rem;
 }
 
 h2 {
   color: var(--colour-ti-highlight);
   font-size: 1.5rem;
+  font-family: metropolis;
 }
 
 .form-field-container {
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-gap: 1.5rem;
-  align-items: center;
-
-  label {
-    width: 100%;
-  }
-
-  .time-format-label {
-    margin-block: 0.25rem;
-  }
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
 }
 
 .start-of-week-bubble-select {
@@ -188,12 +173,6 @@ h2 {
   & > :last-child {
     justify-content: flex-start;
     gap: 0.75rem;
-  }
-}
-
-@media (--md) {
-  .form-field-container {
-    grid-template-columns: 20% 1fr;
   }
 }
 </style>
