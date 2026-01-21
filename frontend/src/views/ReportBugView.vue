664dc9167f5fe56b61334f5e7180fe98e0b4e658
Davi Nakano <davinakanoca@gmail.com>
2026-01-21 13:55:06 -0600

    Remove ReportBug routes and related files

--- frontend/src/views/ReportBugView.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import ExternalRedirect from '@/components/ExternalRedirect.vue';
-
-const redirectUrl = import.meta.env?.VITE_REPORT_BUG_URL;
-
-</script>
-<template>
-  <external-redirect :redirect-url="redirectUrl"/>
-</template>
