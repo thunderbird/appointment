a347b01865399c960259e1500b1489e0c837adcb
Davi Nakano <davinakanoca@gmail.com>
2026-01-22 12:49:10 -0600

    Update LoadingSpinner to use scoped css

--- frontend/src/elements/LoadingSpinner.vue
+++ frontend/src/elements/LoadingSpinner.vue
@@ -1,5 +1,22 @@
 <template>
-  <div
-    class="size-12 animate-spin rounded-full border-4 border-gray-100 !border-t-teal-500 dark:border-gray-600"
-  ></div>
+  <div class="loading-spinner"></div>
 </template>
+
+<style scoped>
+.loading-spinner {
+  width: 3rem;
+  height: 3rem;
+  border-radius: 50%;
+  border-width: 4px;
+  border-style: solid;
+  border-color: var(--colour-neutral-border);
+  border-top-color: var(--colour-primary-default);
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
