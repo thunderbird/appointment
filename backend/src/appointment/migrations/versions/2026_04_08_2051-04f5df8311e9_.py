2d093c0a13c7cf46b1ce931617ba9150c4b96efb
Davi Nakano <davinakanoca@gmail.com>
2026-04-08 14:56:42 -0600

    Run alembic merge head to solve migration branching

---
+++ backend/src/appointment/migrations/versions/2026_04_08_2051-04f5df8311e9_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 04f5df8311e9
+Revises: a1b2c3d4e5f6, b3c4d5e6f7a8
+Create Date: 2026-04-08 20:51:47.740797
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '04f5df8311e9'
+down_revision = ('a1b2c3d4e5f6', 'b3c4d5e6f7a8')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
