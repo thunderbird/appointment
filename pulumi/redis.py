2fb538b564b98a516ccfc260f6063a368bebabab
Ryan Jung <rjung@thunderbird.net>
2026-04-17 15:45:34 -0600

    Lint, change cluster value

--- pulumi/redis.py
+++ pulumi/redis.py
@@ -85,7 +85,7 @@ def redis_cache(
     #    Change DNS to the new replica set in stage
     # -> Change DNS in prod, removing the condition below
 
-    backend_cache_primary_endpoint = backend_cache.endpoints.apply(lambda endpoints: endpoints[0]['address'])
+    backend_cache.endpoints.apply(lambda endpoints: endpoints[0]['address'])
     redis_replica_group_primary_endpoint = redis_replica_group.resources['replication_group'].primary_endpoint_address
     backend_cache_dns = cloudflare.DnsRecord(
         f'{project.name_prefix}-dns-redis',
