e17d6c079fee41971cbba8bb7307b0e40442b52b
Davi Nakano <114549747+davinotdavid@users.noreply.github.com>
2026-05-07 15:37:44 -0600

    Fix LegalView content update toggle + CSS fixes (#1669)
    * CSS fixes for LegalView
    
    * Fix LegalView not reloading content when route changes
    
    * Better padding in LegalView for --md

--- frontend/src/views/LegalView.vue
+++ frontend/src/views/LegalView.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
-import {
-  computed, onMounted, ref,
-} from 'vue';
+import { computed, nextTick, watch } from 'vue';
 import { useRoute } from 'vue-router';
 /* eslint import/no-unresolved: [2, { ignore: ['\\.html'] }] */
 import privacy from '@/assets/legal/en/privacy.html?raw';
@@ -9,45 +7,85 @@ import terms from '@/assets/legal/en/terms.html?raw';
 
 const route = useRoute();
 
-const pageContents = ref('');
 const isTerms = computed(() => route.name === 'terms');
+const pageContents = computed(() => (isTerms.value ? terms : privacy));
 
-onMounted(async () => {
-  if (isTerms.value) {
-    pageContents.value = terms;
-  } else {
-    pageContents.value = privacy;
-  }
-});
+// Since we are using the same component for both privacy and terms routes,
+// we need to use a watch here to scroll to the top when the route changes
+// otherwise the scroll would remain at the bottom
+watch(
+  () => route.fullPath,
+  async () => {
+    await nextTick();
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+    document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+  },
+  { immediate: true },
+);
 </script>
 
 <template>
-  <div class="px-4 md:px-20" v-html="pageContents"></div>
+  <div class="page-contents" v-html="pageContents"></div>
 </template>
 
 <style scoped>
+@import '@/assets/styles/custom-media.pcss';
+
+.page-contents {
+  padding: 0 1rem 1rem;
+
+  @media (--md) {
+    padding: 0 3.75rem 2rem;
+  }
+}
+
 /* Needed for v-html */
 :deep {
-  h1  {
-    @apply py-8 text-3xl text-teal-600 dark:text-teal-400;
+  h1, h2, h3, h4 {
+    color: var(--colour-ti-highlight);
   }
+
+  h1 {
+    padding-top: 2rem;
+    padding-bottom: 2rem;
+    font-size: 1.875rem;
+    line-height: 2.25rem;
+  }
+
   h2 {
-    @apply py-8 text-2xl text-teal-600 dark:text-teal-400;
+    padding-top: 2rem;
+    padding-bottom: 2rem;
+    font-size: 1.5rem;
+    line-height: 2rem;
   }
+
   h3 {
-    @apply py-4 text-xl text-teal-600 dark:text-teal-400;
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    font-size: 1.25rem;
+    line-height: 1.75rem;
   }
+
   h4 {
-    @apply py-4 text-lg text-teal-600 dark:text-teal-400;
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    font-size: 1.125rem;
+    line-height: 1.75rem;
   }
+
   p {
-    @apply py-2 leading-relaxed;
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+    line-height: 1.625;
   }
+
   p:first-of-type {
-    @apply font-semibold;
+    font-weight: 600;
   }
+
   a {
-    @apply underline;
+    text-decoration-line: underline;
   }
 }
 </style>
